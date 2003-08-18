48db57aea654fbd57afb627aa9ff2004358ee0c8
Carl Worth <cworth@cworth.org>
2003-08-18 18:52:46 +0000

    Added better warning if pkg-config is not installed. Add --enable-maintainer-mode to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,21 +3,38 @@
 
 set -e
 
+ARGV0=$0
+
+if test -z "$ACLOCAL_FLAGS"; then
+    acdir=`aclocal --print-ac-dir`
+    if [ ! -f $acdir/pkg.m4 ]; then
+	echo "$ARGV0: Error: Could not find pkg-config macros."
+	echo "        (Looked in $acdir/pkg.m4)"
+	echo "        If pkg.m4 is available in /another/directory, please set"
+	echo "        ACLOCAL_FLAGS=\"-I /another/directory\""
+	echo "        Otherwise, please install pkg-config."
+	echo ""
+	echo "pkg-config is available from:"
+	echo "http://www.freedesktop.org/software/pkgconfig/"
+	exit 1
+    fi
+fi
+
 if test -z "$*"; then
-  echo "$0:	Note: \`./configure' will be run with no arguments."
+  echo "$ARGV0:	Note: \`./configure' will be run with no arguments."
   echo "		If you wish to pass any to it, please specify them on the"
   echo "		\`$0' command line."
   echo
 fi
 
 do_cmd() {
-    echo "$0: running \`$@'"
+    echo "$ARGV0: running \`$@'"
     $@
 }
 
 do_cmd libtoolize --force --copy
 
-do_cmd aclocal
+do_cmd aclocal ${ACLOCAL_FLAGS}
 
 do_cmd autoheader
 
@@ -25,4 +42,4 @@ do_cmd automake --add-missing
 
 do_cmd autoconf
 
-do_cmd ./configure ${1+"$@"} && echo "Now type \`make' to compile" || exit 1
+do_cmd ./configure --enable-maintainer-mode ${1+"$@"} && echo "Now type \`make' to compile" || exit 1
