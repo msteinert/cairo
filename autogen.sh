63468474d732f7e707cafb216051be7802defd00
Dave Beckett <Dave.Beckett@bristol.ac.uk>
2004-07-09 12:47:06 +0000

    Report versions of commands on failure. Try to get version numbers from newer GNU --version formats.

--- autogen.sh
+++ autogen.sh
@@ -24,58 +24,65 @@ libtoolize_min_vers=1.4
 ARGV0=$0
 
 if ($AUTOCONF --version) < /dev/null > /dev/null 2>&1 ; then
-    if ($AUTOCONF --version | awk 'NR==1 { if( $3 >= '$autoconf_min_vers') \
+    if ($AUTOCONF --version | head -1 | awk 'NR==1 { if( $(NF) >= '$autoconf_min_vers') \
 			       exit 1; exit 0; }');
     then
        echo "$ARGV0: ERROR: \`$AUTOCONF' is too old."
+       $AUTOCONF --version
        echo "           (version $autoconf_min_vers or newer is required)"
        DIE="yes"
     fi
 else
+    echo $AUTOCONF: command not found
     echo
-    echo "$ARGV0: ERROR: You must have \`$AUTOCONF' installed to compile $PACKAGE."
+    echo "$ARGV0: ERROR: You must have \`autoconf' installed to compile $PACKAGE."
     echo "           (version $autoconf_min_vers or newer is required)"
     DIE="yes"
 fi
 
 if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 ; then
-  if ($AUTOMAKE --version | awk 'NR==1 { if( $4 >= '$automake_min_vers') \
+  if ($AUTOMAKE --version | head -1 | awk 'NR==1 { if( $(NF) >= '$automake_min_vers') \
 			     exit 1; exit 0; }');
      then
      echo "$ARGV0: ERROR: \`$AUTOMAKE' is too old."
+     $AUTOMAKE --version
      echo "           (version $automake_min_vers or newer is required)"
      DIE="yes"
   fi
   if ($ACLOCAL --version) < /dev/null > /dev/null 2>&1; then
-    if ($ACLOCAL --version | awk 'NR==1 { if( $4 >= '$aclocal_min_vers' ) \
+    if ($ACLOCAL --version | head -1 | awk 'NR==1 { if( $(NF) >= '$aclocal_min_vers' ) \
 						exit 1; exit 0; }' );
     then
       echo "$ARGV0: ERROR: \`$ACLOCAL' is too old."
+      $ACLOCAL --version
       echo "           (version $aclocal_min_vers or newer is required)"
       DIE="yes"
     fi
   else
+    echo $ACLOCAL: command not found
     echo
     echo "$ARGV0: ERROR: Missing \`$ACLOCAL'"
     echo "           The version of $AUTOMAKE installed doesn't appear recent enough."
     DIE="yes"
   fi
 else
+    echo $AUTOMAKE: command not found
     echo
-    echo "$ARGV0: ERROR: You must have \`$AUTOMAKE' installed to compile $PACKAGE."
+    echo "$ARGV0: ERROR: You must have \`automake' installed to compile $PACKAGE."
     echo "           (version $automake_min_vers or newer is required)"
     DIE="yes"
 fi
 
-if (libtoolize --version) < /dev/null > /dev/null 2>&1 ; then
-    if (libtoolize --version | awk 'NR==1 { if( $4 >= '$libtoolize_min_vers') \
+if ($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 ; then
+    if ($LIBTOOLIZE --version | awk 'NR==1 { if( $4 >= '$libtoolize_min_vers') \
 			       exit 1; exit 0; }');
     then
-       echo "$ARGV0: ERROR: \`libtoolize' is too old."
+       echo "$ARGV0: ERROR: \`$LIBTOOLIZE' is too old."
        echo "           (version $libtoolize_min_vers or newer is required)"
        DIE="yes"
     fi
 else
+    echo $LIBTOOLIZE: command not found
     echo
     echo "$ARGV0: ERROR: You must have \`libtoolize' installed to compile $PACKAGE."
     echo "           (version $libtoolize_min_vers or newer is required)"
