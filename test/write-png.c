81d03b3048b238099ae29a4baba84d5870353540
Kristian Høgsberg <krh@redhat.com>
2005-01-27 11:40:29 +0000

    Fix missing rounding in these two functions. Update these to versions with properly rounded alpha values. Move clip_twice out of XFAIL now that rounding works. Add new test cases.

--- test/write-png.c
+++ test/write-png.c
@@ -46,9 +46,9 @@ unpremultiply_data (png_structp png, png_row_infop row_info, png_bytep data)
         if (alpha == 0) {
 	    b[0] = b[1] = b[2] = b[3] = 0;
 	} else {
-            b[0] = (((pixel & 0x0000ff) >>  0) * 255) / alpha;
-            b[1] = (((pixel & 0x00ff00) >>  8) * 255) / alpha;
-            b[2] = (((pixel & 0xff0000) >> 16) * 255) / alpha;
+            b[0] = (((pixel & 0x0000ff) >>  0) * 255 + alpha / 2) / alpha;
+            b[1] = (((pixel & 0x00ff00) >>  8) * 255 + alpha / 2) / alpha;
+            b[2] = (((pixel & 0xff0000) >> 16) * 255 + alpha / 2) / alpha;
 	    b[3] = alpha;
 	}
     }
