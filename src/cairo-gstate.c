811fe63f84983efa7aec984dfb4a4aed365bab02
Tor Lillqvist <tml@novell.com>
2005-03-30 09:31:49 +0000

    src/cairo-gstate.c (_cairo_gstate_stroke_extents): call _cairo_pen_init so that the result takes into account the caps.

--- src/cairo-gstate.c
+++ src/cairo-gstate.c
@@ -1743,6 +1743,8 @@ _cairo_gstate_stroke_extents (cairo_gstate_t *gstate,
     cairo_traps_t traps;
     cairo_box_t extents;
   
+    _cairo_pen_init (&gstate->pen_regular, gstate->line_width / 2.0, gstate);
+
     _cairo_traps_init (&traps);
   
     status = _cairo_path_fixed_stroke_to_traps (&gstate->path, gstate, &traps);
