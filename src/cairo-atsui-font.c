7283ba6d470950e0e49f33fc25f3358113dda51e
Tor Lillqvist <tml@novell.com>
2005-06-21 09:54:58 +0000

    allow building against < 10.3 SDK.

--- src/cairo-atsui-font.c
+++ src/cairo-atsui-font.c
@@ -38,7 +38,19 @@
 #include "cairo-atsui.h"
 #include "cairoint.h"
 #include "cairo.h"
+#if 0
 #include <iconv.h>
+#endif
+
+/*
+ * FixedToFloat/FloatToFixed are 10.3+ SDK items - include definitions
+ * here so we can use older SDKs.
+ */
+#ifndef FixedToFloat
+#define fixed1              ((Fixed) 0x00010000L)
+#define FixedToFloat(a)     ((float)(a) / fixed1)
+#define FloatToFixed(a)     ((Fixed)((float)(a) * fixed1))
+#endif
 
 typedef struct {
     cairo_scaled_font_t base;
