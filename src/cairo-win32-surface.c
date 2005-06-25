13a27d19d4e8748a4233b70cdd4a223865d3d57c
Carl Worth <cworth@cworth.org>
2005-06-25 02:00:17 +0000

    From: J. Ali Harlow" <ali@avrc.city.ac.uk> Reviewed by: Vladimir Vukicevic <vladimirv@gmail.com> Update win32 backend to match current API, so it actually builds once again.

--- src/cairo-win32-surface.c
+++ src/cairo-win32-surface.c
@@ -240,7 +240,6 @@ _create_dc_and_bitmap (cairo_win32_surface_t *surface,
 static cairo_surface_t *
 _cairo_win32_surface_create_for_dc (HDC             original_dc,
 				    cairo_format_t  format,
-				    int	            drawable,
 				    int	            width,
 				    int	            height)
 {
@@ -297,8 +296,7 @@ _cairo_win32_surface_create_similar (void	    *abstract_src,
 {
     cairo_win32_surface_t *src = abstract_src;
 
-    return _cairo_win32_surface_create_for_dc (src->dc, format, drawable,
-					       width, height);
+    return _cairo_win32_surface_create_for_dc (src->dc, format, width, height);
 }
 
 /**
@@ -319,8 +317,7 @@ _cairo_win32_surface_create_dib (cairo_format_t format,
 				 int	        width,
 				 int	        height)
 {
-    return _cairo_win32_surface_create_for_dc (NULL, format, TRUE,
-					       width, height);
+    return _cairo_win32_surface_create_for_dc (NULL, format, width, height);
 }
 
 static cairo_status_t
@@ -577,7 +574,6 @@ _cairo_win32_surface_composite (cairo_operator_t	operator,
     } else if (integer_transform &&
 	       (src->format == CAIRO_FORMAT_RGB24 || src->format == CAIRO_FORMAT_ARGB32) &&
 	       dst->format == CAIRO_FORMAT_RGB24 &&
-	       !src->base.repeat &&
 	       operator == CAIRO_OPERATOR_OVER) {
 
 	BLENDFUNCTION blend_function;
@@ -675,6 +671,9 @@ categorize_solid_dest_operator (cairo_operator_t operator,
 	    return DO_UNSUPPORTED;
 	break;
     }	
+
+    ASSERT_NOT_REACHED;
+    return DO_UNSUPPORTED;
 }
 
 static cairo_int_status_t
@@ -751,8 +750,12 @@ _cairo_win32_surface_set_clip_region (void              *abstract_surface,
     /* If we are in-memory, then we set the clip on the image surface
      * as well as on the underlying GDI surface.
      */
-    if (surface->image)
-	_cairo_surface_set_clip_region (surface->image, region);
+    if (surface->image) {
+	unsigned int serial;
+
+	serial = _cairo_surface_allocate_clip_serial (surface->image);
+	_cairo_surface_set_clip_region (surface->image, region, serial);
+    }
 
     /* The semantics we want is that any clip set by cairo combines
      * is intersected with the clip on device context that the
