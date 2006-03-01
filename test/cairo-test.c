6197d5a7618900f25155af16b91f082ee05ddd03
Carl Worth <cworth@cworth.org>
2006-03-01 11:54:21 +0100

    Fix bug in test suite causing PS and PDF to fail with rgb24 and device offsets.
    Patch from cworth@cworth.org.
    
    The trick with this one is that the test suite is using device offsets
    in a rather fabricated way, and the final copy from an offset surface
    to a non-offset surface is between two surfaces of the same size, so
    must be 1:1. To get this we turn the device offset off before the
    copy.
    
    In a more conventional use of device offsets the surfaces would be of
    different sizes, and the offset would be desired during the copy.

--- test/cairo-test.c
+++ test/cairo-test.c
@@ -1181,6 +1181,15 @@ ps_surface_write_to_png (cairo_surface_t *surface, const char *filename)
     ps_target_closure_t *ptc = cairo_surface_get_user_data (surface, &ps_closure_key);
     char    command[4096];
 
+    /* Both surface and ptc->target were originally created at the
+     * same dimensions. We want a 1:1 copy here, so we first clear any
+     * device offset on surface.
+     *
+     * In a more realistic use case of device offsets, the target of
+     * this copying would be of a different size than the source, and
+     * the offset would be desirable during the copy operation. */
+    cairo_surface_set_device_offset (surface, 0, 0);
+
     if (ptc->target) {
 	cairo_t *cr;
 	cr = cairo_create (ptc->target);
@@ -1272,6 +1281,15 @@ pdf_surface_write_to_png (cairo_surface_t *surface, const char *filename)
 {
     pdf_target_closure_t *ptc = cairo_surface_get_user_data (surface, &pdf_closure_key);
     char    command[4096];
+
+    /* Both surface and ptc->target were originally created at the
+     * same dimensions. We want a 1:1 copy here, so we first clear any
+     * device offset on surface.
+     *
+     * In a more realistic use case of device offsets, the target of
+     * this copying would be of a different size than the source, and
+     * the offset would be desirable during the copy operation. */
+    cairo_surface_set_device_offset (surface, 0, 0);
 
     if (ptc->target) {
 	cairo_t *cr;
