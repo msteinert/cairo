bdc1c1ac783f1d2bda742ea7150de2c4b0751a29
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2006-05-01 22:12:20 +0200

    Copy content property when doing a snapshot of a meta surface.

--- src/cairo-meta-surface.c
+++ src/cairo-meta-surface.c
@@ -483,6 +483,7 @@ _cairo_meta_surface_snapshot (void *abstract_other)
     meta->width_pixels = other->width_pixels;
     meta->height_pixels = other->height_pixels;
     meta->replay_start_idx = other->replay_start_idx;
+    meta->content = other->content;
 
     _cairo_array_init_snapshot (&meta->commands, &other->commands);
     meta->commands_owner = cairo_surface_reference (&other->base);
