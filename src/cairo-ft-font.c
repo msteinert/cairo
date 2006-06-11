4af28e639dcf7fe3332c0302295e6d590a505af7
Jinghua Luo <sunmoon1997@gmail.com>
2006-06-11 18:16:46 +0800

    ignore FC_MATRIX in font pattern.
    With previous commit, freetype font backend is able to transform
    both bitmap and outline glyphs, so ignores FC_MATRIX from now,
    cairo doesn't need this for artificial oblique. And cairo doesn't
    use FC_MATRIX direct anyway, it has its own font matrix that may be not
    equal to FC_MATRIX in the font pattern! You should pass the matrix
    (usually the matrix is multiplied by font's pixel size) to cairo when
    creating scaled font.

--- src/cairo-ft-font.c
+++ src/cairo-ft-font.c
@@ -1230,8 +1230,6 @@ static cairo_ft_options_t
 _get_pattern_ft_options (FcPattern *pattern)
 {
     FcBool antialias, vertical_layout, hinting, autohint, bitmap;
-    FcBool transform;
-    FcMatrix *font_matrix;
     cairo_ft_options_t ft_options;
     int rgba;
 #ifdef FC_HINT_STYLE
@@ -1246,13 +1244,6 @@ _get_pattern_ft_options (FcPattern *pattern)
 #define FC_EMBEDDED_BITMAP "embeddedbitmap"
 #endif
 
-    if (FcPatternGetMatrix (pattern,
-			    FC_MATRIX, 0, &font_matrix) != FcResultMatch)
-	font_matrix = NULL;
-
-    transform = (font_matrix && (font_matrix->xx != 1 || font_matrix->xy != 0 ||
-				 font_matrix->yx != 0 || font_matrix->yy != 1));
-
     /* Check whether to force use of embedded bitmaps */
     if (FcPatternGetBool (pattern,
 			  FC_EMBEDDED_BITMAP, 0, &bitmap) != FcResultMatch)
@@ -1263,7 +1254,7 @@ _get_pattern_ft_options (FcPattern *pattern)
 			  FC_ANTIALIAS, 0, &antialias) != FcResultMatch)
 	antialias = FcTrue;
 
-    if ((!bitmap && antialias) || transform)
+    if (!bitmap && antialias)
 	ft_options.load_flags |= FT_LOAD_NO_BITMAP;
     else if (!antialias)
 	ft_options.load_flags |= FT_LOAD_MONOCHROME;
