29f483326f127ec60e26801732da2486eb84b7ef
Michael Emmel <memmel@debian.localdomain>
2006-10-12 20:39:16 -0700

    Fixed test to compile

--- boilerplate/cairo-test-directfb.c
+++ boilerplate/cairo-test-directfb.c
@@ -11,8 +11,7 @@ make check
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "cairo-test.h"
-#include "cairo-test-directfb.h"
+#include "cairo-boilerplate.h"
 #include <directfb.h>
 #include "cairo-directfb.h"
 
@@ -37,7 +36,7 @@ static DFBInfo *init(void) {
 	DFBDisplayLayerConfig        layer_config;
 	DFBGraphicsDeviceDescription desc;
 	int err;
-	DFBInfo *info = malloc(sizeof(DFBInfo));
+	DFBInfo *info = calloc(1,sizeof(DFBInfo));
 	if( !info )
 		return NULL;
 
@@ -58,25 +57,24 @@ static DFBInfo *init(void) {
 	return info;
 }
 
-cairo_surface_t *
-create_directfb_surface (cairo_test_t* test, cairo_format_t format,
-                     void **closure) {
+static cairo_surface_t *
+create_directfb_window_surface (DFBInfo *info,
+		     cairo_content_t		  content,
+		     int			  width,
+		     int			  height) {
 	DFBWindowDescription desc;
 	int err;
-	DFBInfo *info = init();
-	if( !info )
-		return NULL;
-	*closure = info;
-
 	desc.flags  = ( DWDESC_POSX | DWDESC_POSY |
-			DWDESC_WIDTH | DWDESC_HEIGHT | DWDESC_CAPS|DSDESC_PIXELFORMAT );
+			DWDESC_WIDTH | DWDESC_HEIGHT /*| DWDESC_CAPS|DSDESC_PIXELFORMAT*/ );
 	desc.posx   = 0;
 	desc.posy   = 0;
-	desc.width  = test->width;
-	desc.height = test->height;
+	desc.width  = width;
+	desc.height = height;
+#if 0 /*Test using native format by default*/
 	desc.caps =  DWCAPS_DOUBLEBUFFER;
 	desc.caps |= DWCAPS_ALPHACHANNEL;
 	desc.pixelformat = DSPF_ARGB;
+#endif
 
 	DFBCHECK(info->layer->CreateWindow( info->layer, &desc, &info->window ) );
 	info->window->SetOpacity( info->window, 0xFF );
@@ -84,29 +82,24 @@ create_directfb_surface (cairo_test_t* test, cairo_format_t format,
 	info->surface->SetColor( info->surface, 0xFF, 0xFF, 0xFF, 0xFF );
 	info->surface->FillRectangle( info->surface,0, 0, desc.width, desc.height );
 	info->surface->Flip( info->surface, NULL, 0 );
-	cairo_surface_t *csurface=cairo_directfb_surface_create(info->dfb,info->surface);
-	return csurface;
+	return cairo_directfb_surface_create(info->dfb,info->surface);
 }
 
-cairo_surface_t *
-create_directfb_bitmap_surface (cairo_test_t* test, cairo_format_t format,
-                            void **closure) {
+static cairo_surface_t *
+create_directfb_bitmap_surface (DFBInfo *info,
+		     cairo_content_t		  content,
+		     int			  width,
+		     int			  height) {
 	int  err;
-	DFBInfo *info = init();
-	if( !info ) return NULL;
-	*closure = info;
-	info->window= NULL; /* make sure window is null*/
 	DFBSurfaceDescription  desc;
 
 	desc.flags = DSDESC_WIDTH | DSDESC_HEIGHT | DSDESC_PIXELFORMAT;
 	desc.caps = DSCAPS_NONE;
-	desc.width  = test->width;
-	desc.height = test->height;
+	desc.width  = width;
+	desc.height = height;
 	desc.pixelformat = DSPF_ARGB;
 	DFBCHECK(info->dfb->CreateSurface (info->dfb, &desc,&info->surface));
-	cairo_surface_t *csurface=cairo_directfb_surface_create(info->dfb,
-		info->surface);
-	return csurface;
+	return cairo_directfb_surface_create(info->dfb,info->surface);
 }
 
 void
@@ -120,4 +113,30 @@ cleanup_directfb (void* closure) {
 		info->layer->Release( info->layer );
 	if( info->dfb )
 		info->dfb->Release( info->dfb );
+}
+
+cairo_surface_t *
+create_directfb_surface (const char			 *name,
+		     cairo_content_t		  content,
+		     int			  width,
+		     int			  height,
+		     cairo_boilerplate_mode_t	  mode,
+		     void			**closure) {
+    
+    DFBInfo* info= init();
+    *closure = info;
+    if( !info ) { 
+	    CAIRO_BOILERPLATE_LOG ("Failed to init directfb:\n");
+        return NULL;
+    }
+
+    if (width == 0)
+	width = 1;
+    if (height == 0)
+	height = 1;
+
+    if (mode == CAIRO_BOILERPLATE_MODE_TEST)
+	return create_directfb_bitmap_surface (info, content, width, height);
+    else /* mode == CAIRO_BOILERPLATE_MODE_PERF */
+	return create_directfb_window_surface (info, content, width, height);
 }
