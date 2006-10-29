bc983700c463671128822e2eb9f74cbe834a1c1b
Behdad Esfahbod <behdad@behdad.org>
2006-10-29 14:49:06 -0500

    [autogen.sh] Make sed script portable

--- autogen.sh
+++ autogen.sh
@@ -36,7 +36,7 @@ if test "X$CONFIGURE_IN" = X; then
 fi
 
 extract_version() {
-	grep "^ *$1" $CONFIGURE_IN | sed 's/.*(\[\?\([^])]\+\)]\?).*/\1/'
+	grep "^ *$1" $CONFIGURE_IN | sed 's/.*(\[*\([^])]*\)]*).*/\1/'
 }
 
 autoconf_min_vers=`extract_version AC_PREREQ`
