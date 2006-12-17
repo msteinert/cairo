0bea2ce7f7e9c4b46d1125b07ed719fbb8b7102c
Behdad Esfahbod <behdad@behdad.org>
2006-12-17 14:31:39 -0500

    [pdiff] Define _GNU_SOURCE to get correct symbols out of <math.h>
    math.h does not define __USE_ISOC99 otherwise.

--- test/pdiff/pdiff.c
+++ test/pdiff/pdiff.c
@@ -14,6 +14,8 @@
   if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+#define _GNU_SOURCE
+
 #include "lpyramid.h"
 #include <math.h>
 #include <stdio.h>
