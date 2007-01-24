50136a78063137ebca16bcb839bec33e2917824e
Eugeniy Meshcheryakov <eugen@debian.org>
2007-01-24 15:51:00 -0500

    [PDF-CFF] Fix bad /LastChar value for CFF fonts in PDF backend
    PDF backend sets /LastChar value in Type1C font dictionary incorrectly.
    acroread then complains about incorrect /Widths in font.
    The last char is numchars - 1, since it's zero-based.

--- src/cairo-pdf-surface.c
+++ src/cairo-pdf-surface.c
@@ -1709,7 +1709,7 @@ _cairo_pdf_surface_emit_cff_font_subset (cairo_pdf_surface_t		*surface,
 				 "   /Widths [",
 				 subset_resource.id,
 				 subset.base_font,
-				 font_subset->num_glyphs,
+				 font_subset->num_glyphs - 1,
 				 descriptor.id);
 
     for (i = 0; i < font_subset->num_glyphs; i++)
