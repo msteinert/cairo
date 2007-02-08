4773b8a6d25e4b228471ad48616c08367ee84809
Peter Weilbacher <pmw@avila.aip.de>
2007-02-08 20:34:26 +0100

    Oops, correct typo in previous commit
    FALSA -> FALSE

--- src/cairoint.h
+++ src/cairoint.h
@@ -179,7 +179,7 @@ typedef CRITICAL_SECTION cairo_mutex_t;
 # define CAIRO_MUTEX_LOCK(name) DosRequestMutexSem(name, SEM_INDEFINITE_WAIT)
 # define CAIRO_MUTEX_UNLOCK(name) DosReleaseMutexSem(name)
 typedef HMTX cairo_mutex_t;
-# define CAIRO_MUTEX_INIT(mutex) DosCreateMutexSem (NULL, mutex, 0L, FALSA)
+# define CAIRO_MUTEX_INIT(mutex) DosCreateMutexSem (NULL, mutex, 0L, FALSE)
 # define CAIRO_MUTEX_FINI(mutex) DosCloseMutexSem (*(mutex))
 # define CAIRO_MUTEX_NIL_INITIALIZER 0
 #endif
