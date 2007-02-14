msteinert/cairo

commit 25a370d7992d7f70d8cf79f1b328e40437b40ac4

Avoid public cairo_ft_scaled_font_lock_face for internal use
We're planning to change the implementation of the public function,
(which will remove some locking safety), so use the safe, locked
_cairo_ft_unscaled_font_lock_face for internal use instead.

src/cairo-ft-font.c
@@ -1769,7 +1769,7 @@ _cairo_ft_scaled_glyph_init (void			*abstract_font,
     cairo_bool_t vertical_layout = FALSE;
     cairo_status_t status = CAIRO_STATUS_SUCCESS;
 
-    face = cairo_ft_scaled_font_lock_face (abstract_font);
+    face = _cairo_ft_unscaled_font_lock_face (unscaled);
     if (!face)
 	return CAIRO_STATUS_NO_MEMORY;
 
@@ -1935,7 +1935,7 @@ _cairo_ft_scaled_glyph_init (void			*abstract_font,
 				   load_flags | FT_LOAD_NO_BITMAP);
 
 	    if (error) {
-		cairo_ft_scaled_font_unlock_face (abstract_font);
+		_cairo_ft_unscaled_font_unlock_face (unscaled);
 		return CAIRO_STATUS_NO_MEMORY;
 	    }
 #if HAVE_FT_GLYPHSLOT_EMBOLDEN
@@ -1963,7 +1963,7 @@ _cairo_ft_scaled_glyph_init (void			*abstract_font,
 				      path);
     }
  FAIL:
-    cairo_ft_scaled_font_unlock_face (abstract_font);
+    _cairo_ft_unscaled_font_unlock_face (unscaled);
 
     return status;
 }

commit 7adab2866be21207a15f07aab01fb17edd0e661f

Add missing call to set_scale of unscaled_font in scaled_glyph init
This fixes a regression introduced with commit:

	25a370d7992d7f70d8cf79f1b328e40437b40ac4

This was showing up as a failure of the bitmap-font test case
with the ps, pdf, and svg backends.

src/cairo-ft-font.c
@@ -1782,6 +1782,9 @@ _cairo_ft_scaled_glyph_init (void			*abstract_font,
     if (!face)
 	return CAIRO_STATUS_NO_MEMORY;
 
+    _cairo_ft_unscaled_font_set_scale (scaled_font->unscaled,
+				       &scaled_font->base.scale);
+
     /* Ignore global advance unconditionally */
     load_flags |= FT_LOAD_IGNORE_GLOBAL_ADVANCE_WIDTH;
 