a1287e483ce8efcf3f1eca97bdaaccac2393a1bf
Behdad Esfahbod <behdad@behdad.org>
2007-04-03 20:30:30 -0400

    [pixman/src/check-config.sh] Test that all source files #include <config.h> as their first include.

--- pixman/src/check-config.sh
+++ pixman/src/check-config.sh
@@ -5,13 +5,13 @@ LANG=C
 test -z "$srcdir" && srcdir=.
 status=0
 
-echo 'Checking source files for missing or misplaced #include "cairoint.h"'
+echo 'Checking source files for missing or misplaced #include <config.h>'
 
 find "$srcdir" -name '*.c' -or -name '*.cpp' |
 while read x; do
 	grep '\<include\>' "$x" /dev/null | head -n 1
 done |
-grep -v '"cairoint.h"' |
+grep -v '<config.h>' |
 grep . && status = 1
 
 exit $status
