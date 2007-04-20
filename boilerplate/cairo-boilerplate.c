06b657ccf129f6f154c0db7886e5cca6191f93ca
Behdad Esfahbod <behdad@behdad.org>
2007-04-19 22:20:38 -0400

    [boilerplate] Strip xcb boilerplate into cairo-boilerplate-xcb*

--- boilerplate/cairo-boilerplate.c
+++ boilerplate/cairo-boilerplate.c
@@ -65,6 +65,9 @@
 #if CAIRO_HAS_XLIB_XRENDER_SURFACE
 #include "cairo-boilerplate-xlib-private.h"
 #endif
+#if CAIRO_HAS_XCB_SURFACE
+#include "cairo-boilerplate-xcb-private.h"
+#endif
 
 /* This is copied from cairoint.h. That makes it painful to keep in
  * sync, but the slim stuff makes cairoint.h "hard" to include when
@@ -751,93 +754,6 @@ create_win32_surface (const char		 *name,
 static void
 cleanup_win32 (void *closure)
 {
-}
-#endif
-
-#if CAIRO_HAS_XCB_SURFACE
-#include "cairo-xcb-xrender.h"
-#include <xcb/xcb_renderutil.h>
-typedef struct _xcb_target_closure
-{
-    xcb_connection_t *c;
-    xcb_pixmap_t pixmap;
-} xcb_target_closure_t;
-
-static void
-_cairo_boilerplate_xcb_synchronize (void *closure)
-{
-    xcb_target_closure_t *xtc = closure;
-    free (xcb_get_image_reply (xtc->c,
-		xcb_get_image (xtc->c, XCB_IMAGE_FORMAT_Z_PIXMAP,
-		    xtc->pixmap, 0, 0, 1, 1, /* AllPlanes */ ~0UL),
-		0));
-}
-
-static cairo_surface_t *
-_cairo_boilerplate_xcb_create_surface (const char		 *name,
-				       cairo_content_t		  content,
-				       int			  width,
-				       int			  height,
-				       cairo_boilerplate_mode_t	  mode,
-				       void			**closure)
-{
-    xcb_screen_t *root;
-    xcb_target_closure_t *xtc;
-    cairo_surface_t *surface;
-    xcb_connection_t *c;
-    xcb_render_pictforminfo_t *render_format;
-    xcb_pict_standard_t format;
-
-    *closure = xtc = xmalloc (sizeof (xcb_target_closure_t));
-
-    if (width == 0)
-	width = 1;
-    if (height == 0)
-	height = 1;
-
-    xtc->c = c = xcb_connect(NULL,NULL);
-    if (xcb_connection_has_error(c)) {
-	CAIRO_BOILERPLATE_LOG ("Failed to connect to X server through XCB\n");
-	return NULL;
-    }
-
-    root = xcb_setup_roots_iterator(xcb_get_setup(c)).data;
-
-    xtc->pixmap = xcb_generate_id (c);
-    xcb_create_pixmap (c, 32, xtc->pixmap, root->root,
-			 width, height);
-
-    switch (content) {
-    case CAIRO_CONTENT_COLOR:
-	format = XCB_PICT_STANDARD_RGB_24;
-	break;
-    case CAIRO_CONTENT_COLOR_ALPHA:
-	format = XCB_PICT_STANDARD_ARGB_32;
-	break;
-    case CAIRO_CONTENT_ALPHA:  /* would be XCB_PICT_STANDARD_A_8 */
-    default:
-	CAIRO_BOILERPLATE_LOG ("Invalid content for XCB test: %d\n", content);
-	return NULL;
-    }
-
-    render_format = xcb_render_util_find_standard_format (xcb_render_util_query_formats (c), format);
-    if (render_format->id == 0)
-	return NULL;
-    surface = cairo_xcb_surface_create_with_xrender_format (c, xtc->pixmap, root,
-							    render_format,
-							    width, height);
-
-    return surface;
-}
-
-static void
-_cairo_boilerplate_xcb_cleanup (void *closure)
-{
-    xcb_target_closure_t *xtc = closure;
-
-    xcb_free_pixmap (xtc->c, xtc->pixmap);
-    xcb_disconnect (xtc->c);
-    free (xtc);
 }
 #endif
 
