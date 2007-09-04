e1819a24fb0196218c4045616c2ba458819b7d31
Behdad Esfahbod <behdad@behdad.org>
2007-09-04 19:12:14 -0400

    [cairo-xlib-xrender-private] Remove some protocol-specific macros

--- src/cairo-xlib-xrender-private.h
+++ src/cairo-xlib-xrender-private.h
@@ -152,54 +152,6 @@ typedef unsigned long	GlyphSet;
 typedef unsigned long	Picture;
 typedef unsigned long	PictFormat;
 
-#define RENDER_NAME	"RENDER"
-#define RENDER_MAJOR	0
-#define RENDER_MINOR	10
-
-#define X_RenderQueryVersion		    0
-#define X_RenderQueryPictFormats	    1
-#define X_RenderQueryPictIndexValues	    2	/* 0.7 */
-#define X_RenderQueryDithers		    3
-#define X_RenderCreatePicture		    4
-#define X_RenderChangePicture		    5
-#define X_RenderSetPictureClipRectangles    6
-#define X_RenderFreePicture		    7
-#define X_RenderComposite		    8
-#define X_RenderScale			    9
-#define X_RenderTrapezoids		    10
-#define X_RenderTriangles		    11
-#define X_RenderTriStrip		    12
-#define X_RenderTriFan			    13
-#define X_RenderColorTrapezoids		    14
-#define X_RenderColorTriangles		    15
-/* #define X_RenderTransform		    16 */
-#define X_RenderCreateGlyphSet		    17
-#define X_RenderReferenceGlyphSet	    18
-#define X_RenderFreeGlyphSet		    19
-#define X_RenderAddGlyphs		    20
-#define X_RenderAddGlyphsFromPicture	    21
-#define X_RenderFreeGlyphs		    22
-#define X_RenderCompositeGlyphs8	    23
-#define X_RenderCompositeGlyphs16	    24
-#define X_RenderCompositeGlyphs32	    25
-#define X_RenderFillRectangles		    26
-/* 0.5 */
-#define X_RenderCreateCursor		    27
-/* 0.6 */
-#define X_RenderSetPictureTransform	    28
-#define X_RenderQueryFilters		    29
-#define X_RenderSetPictureFilter	    30
-/* 0.8 */
-#define X_RenderCreateAnimCursor	    31
-/* 0.9 */
-#define X_RenderAddTraps		    32
-/* 0.10 */
-#define X_RenderCreateSolidFill             33
-#define X_RenderCreateLinearGradient        34
-#define X_RenderCreateRadialGradient        35
-#define X_RenderCreateConicalGradient       36
-#define RenderNumberRequests		    (X_RenderCreateConicalGradient+1)
-
 #define BadPictFormat			    0
 #define BadPicture			    1
 #define BadPictOp			    2
