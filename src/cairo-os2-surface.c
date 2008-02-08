0626cac6dba91d0ee20458858275745b7c6a1bfd
Peter Weilbacher <mozilla@weilbacher.org>
2008-02-08 15:09:19 +0100

    [os2] fix typo in comment

--- src/cairo-os2-surface.c
+++ src/cairo-os2-surface.c
@@ -136,7 +136,7 @@ cairo_os2_fini (void)
     DisableFPUException ();
 
     /* Free allocated memories! */
-    /* (Check cairo_debug_reset_static_date () for an example of this!) */
+    /* (Check cairo_debug_reset_static_data () for an example of this!) */
     _cairo_font_reset_static_data ();
 #if CAIRO_HAS_FT_FONT
     _cairo_ft_font_reset_static_data ();
