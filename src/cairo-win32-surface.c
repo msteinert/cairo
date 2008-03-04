8ba8a1192497ff89215f8a1657cbe88609083fb1
Chris Wilson <chris@chris-wilson.co.uk>
2008-03-04 13:59:57 +0000

    [win32] make check doc fixup.
    Add the missing '%' to the macro as demanded by make check.

--- src/cairo-win32-surface.c
+++ src/cairo-win32-surface.c
@@ -1678,7 +1678,7 @@ _cairo_win32_surface_show_glyphs (void			*surface,
  * Creates a cairo surface that targets the given DC.  The DC will be
  * queried for its initial clip extents, and this will be used as the
  * size of the cairo surface.  The resulting surface will always be of
- * format CAIRO_FORMAT_RGB24; should you need another surface format,
+ * format %CAIRO_FORMAT_RGB24; should you need another surface format,
  * you will need to create one through
  * cairo_win32_surface_create_with_dib().
  *
