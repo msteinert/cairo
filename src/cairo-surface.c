eb63e5609ebb2b666f0cce552dec4c335f210baf
Behdad Esfahbod <behdad@behdad.org>
2008-05-09 13:00:28 +0200

    [_cairo_surface_set_device_scale] zero xy and yx matrix entries
    Just in case...

--- src/cairo-surface.c
+++ src/cairo-surface.c
@@ -785,9 +785,13 @@ _cairo_surface_set_device_scale (cairo_surface_t *surface,
 
     surface->device_transform.xx = sx;
     surface->device_transform.yy = sy;
+    surface->device_transform.xy = 0.0;
+    surface->device_transform.yx = 0.0;
 
     surface->device_transform_inverse.xx = 1.0 / sx;
     surface->device_transform_inverse.yy = 1.0 / sy;
+    surface->device_transform_inverse.xy = 0.0;
+    surface->device_transform_inverse.yx = 0.0;
 }
 
 /**
