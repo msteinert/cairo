ce56e0cd0c0afc887bc0c4d46d51b934923f2d2c
Adrian Johnson <ajohnson@redneon.com>
2008-05-27 12:44:50 +0930

    Add missing argument to _cairo_pdf_surface_show_glyphs()
    cf473f4a75ca0d3c815222287d7c144e72de5add missed the PDF surface when
    it added an argument to the show_glyphs() surface backend function.

--- src/cairo-pdf-surface.c
+++ src/cairo-pdf-surface.c
@@ -4638,7 +4638,8 @@ _cairo_pdf_surface_show_glyphs (void			*abstract_surface,
 				cairo_pattern_t		*source,
 				cairo_glyph_t		*glyphs,
 				int			 num_glyphs,
-				cairo_scaled_font_t	*scaled_font)
+				cairo_scaled_font_t	*scaled_font,
+				int		        *remaining_glyphs)
 {
     cairo_pdf_surface_t *surface = abstract_surface;
     cairo_status_t status;
