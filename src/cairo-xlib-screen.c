64ba6c74dbc5464da3093867b4f26166da7fbaff
Behdad Esfahbod <behdad@behdad.org>
2008-08-06 18:37:03 -0400

    Fix Ubuntu-specific issue with FC_LCD_NONE et al
    No idea if Ubuntu will ever learn not to modify public API of
    common libraries without any clue of what kind of problems they
    create by doing that.  They could very well define
    FC_UBUNTU_LCD_FILTER if they wanted to, but no, they defined
    FC_LCD_FILTER as if it's an upstream thing.  It wasn't.

--- src/cairo-xlib-screen.c
+++ src/cairo-xlib-screen.c
@@ -135,6 +135,9 @@ get_integer_default (Display    *dpy,
 /* Fontconfig version older than 2.6 didn't have these options */
 #ifndef FC_LCD_FILTER
 #define FC_LCD_FILTER	"lcdfilter"
+#endif
+/* Stupid Ubuntu defined FC_LCD_FILTER without defining the following */
+#ifndef FC_LCD_NONE
 #define FC_LCD_NONE	0
 #define FC_LCD_DEFAULT	1
 #define FC_LCD_LIGHT	2
