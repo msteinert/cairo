7a0f29d2f35e31ad30f7dc67ec3e5c3ee06fec53
Chris Wilson <chris@chris-wilson.co.uk>
2008-08-14 11:57:36 +0100

    [test] Update prototype of win32-printing create_surface()
    Missed the recent addition of the win32-printing surface to boilerplate.

--- boilerplate/cairo-boilerplate-win32-private.h
+++ boilerplate/cairo-boilerplate-win32-private.h
@@ -39,12 +39,15 @@ _cairo_boilerplate_win32_create_surface (const char			 *name,
 					 void				**closure);
 
 cairo_surface_t *
-_cairo_boilerplate_win32_printing_create_surface (const char 		  *name,
-						  cairo_content_t 	   content,
-						  int 			   width,
-						  int 			   height,
+_cairo_boilerplate_win32_printing_create_surface (const char		  *name,
+						  cairo_content_t	   content,
+						  int			   width,
+						  int			   height,
+						  int			   max_width,
+						  int			   max_height,
 						  cairo_boilerplate_mode_t mode,
-						  void 			 **closure);
+						  int			   id,
+						  void			 **closure);
 
 void
 _cairo_boilerplate_win32_printing_cleanup (void *closure);
