2ab700f21af7a98d82e74fd4d04884b2b0ca1b20
Chris Wilson <chris@chris-wilson.co.uk>
2008-08-17 14:59:53 +0100

    [type3-glyph] Propagate error from image emission.
    Return the error status from ->emit_image().

--- src/cairo-type3-glyph-surface.c
+++ src/cairo-type3-glyph-surface.c
@@ -112,7 +112,7 @@ _cairo_type3_glyph_surface_emit_image (cairo_type3_glyph_surface_t *surface,
     if (image_mask != image)
 	cairo_surface_destroy (&image_mask->base);
 
-    return CAIRO_STATUS_SUCCESS;
+    return status;
 }
 
 static cairo_status_t
