98942f99f3e10a1898a11d29e5f40f894d74c0a7
Behdad Esfahbod <behdad@behdad.org>
2008-08-27 17:53:02 -0400

    [configure.in] Move library version information to Version.mk

---
+++ Version.mk
@@ -0,0 +1,10 @@
+#
+# cairo package version number, (as distinct from shared library version)
+#
+# For the minor number: odd  => unstable series
+#			even => stable series
+# For the micro number: odd  => in-progress development (from git)
+#			even => tar-file snapshot or release
+cairo_version_major=1
+cairo_version_minor=7
+cairo_version_micro=5
