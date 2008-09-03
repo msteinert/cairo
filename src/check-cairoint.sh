2823ebaa6e0a14c4270ec973467fc0f76dc34fd5
Behdad Esfahbod <behdad@behdad.org>
2008-09-02 20:59:38 -0400

    [src/check-link.c] Simple test to check we can link to the built cairo.so

--- src/check-cairoint.sh
+++ src/check-cairoint.sh
@@ -7,7 +7,7 @@ stat=0
 
 echo 'Checking source files for missing or misplaced #include "cairoint.h"'
 
-find "$srcdir" -name '*.c' -or -name '*.cpp' |
+find "$srcdir" \( -name '*.c' -or -name '*.cpp' \) -and ! -name 'check-*.c' |
 while read x; do
 	grep '\<include\>' "$x" /dev/null | head -n 1
 done |
