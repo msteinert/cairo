5d887ad5dca5af0f8216830d1b04d08a5aba9bee
Carl Worth <cworth@cworth.org>
2008-09-25 13:31:20 -0700

    Remove all lcd_filter code.
    We reverted the public API for setting lcd_filter font options
    back in 1b42bc8033bc , but we had left the implementation which
    would examine fontconfig and Xft properties for the option, and
    which would call into freetype for subpixel glyph rasterization.
    
    However, I recently realized, (and the test suite had been trying
    to tell me for a while), that this approach would cause a
    regression for users who were previously using sub-pixel text,
    but without sub-pixel rendering built directly into freetype.
    That's not acceptable, so all the code is coming out for now.

--- src/cairo-surface.c
+++ src/cairo-surface.c
@@ -73,7 +73,6 @@ const cairo_surface_t name = {					\
     FALSE,				/* has_font_options */	\
     { CAIRO_ANTIALIAS_DEFAULT,		/* antialias */		\
       CAIRO_SUBPIXEL_ORDER_DEFAULT,	/* subpixel_order */	\
-      CAIRO_LCD_FILTER_DEFAULT,		/* lcd_filter */	\
       CAIRO_HINT_STYLE_DEFAULT,		/* hint_style */	\
       CAIRO_HINT_METRICS_DEFAULT	/* hint_metrics */	\
     }					/* font_options */	\
