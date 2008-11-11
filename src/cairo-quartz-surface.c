msteinert/cairo

commit 794460186459981cd43ca38f3eba07e3f6bf8908

[pattern] Avoid needless copying of patterns.
Only copy the pattern if we need to modify it, e.g. preserve a copy in a
snapshot or a soft-mask, or to modify the matrix. Otherwise we can
continue to use the original pattern and mark it as const in order to
generate compiler warnings if we do attempt to write to it.

src/cairo-quartz-surface.c
@@ -688,7 +688,7 @@ CreateGradientFunction (cairo_gradient_pattern_t *gpat)
 	0, ComputeGradientValue, (CGFunctionReleaseInfoCallback) cairo_pattern_destroy
     };
 
-    return CGFunctionCreate (gpat,
+    return CGFunctionCreate (_cairo_pattern_create_copy (&gpat->base),
 			     1,
 			     input_value_range,
 			     4,
@@ -766,7 +766,7 @@ CreateRepeatingGradientFunction (cairo_quartz_surface_t *surface,
     input_value_range[0] = 0.0 - 1.0 * rep_start;
     input_value_range[1] = 1.0 + 1.0 * rep_end;
 
-    return CGFunctionCreate (gpat,
+    return CGFunctionCreate (_cairo_pattern_create_copy (&gpat->base),
 			     1,
 			     input_value_range,
 			     4,
@@ -1096,9 +1096,6 @@ _cairo_quartz_setup_linear_source (cairo_quartz_surface_t *surface,
     end = CGPointMake (_cairo_fixed_to_double (lpat->p2.x),
 		       _cairo_fixed_to_double (lpat->p2.y));
 
-    // ref will be released by the CGShading's destructor
-    cairo_pattern_reference ((cairo_pattern_t*) lpat);
-
     if (abspat->extend == CAIRO_EXTEND_NONE ||
 	abspat->extend == CAIRO_EXTEND_PAD)
     {
@@ -1158,9 +1155,6 @@ _cairo_quartz_setup_radial_source (cairo_quartz_surface_t *surface,
 			 _cairo_fixed_to_double (rpat->c1.y));
     end = CGPointMake (_cairo_fixed_to_double (rpat->c2.x),
 		       _cairo_fixed_to_double (rpat->c2.y));
-
-    // ref will be released by the CGShading's destructor
-    cairo_pattern_reference ((cairo_pattern_t*) rpat);
 
     gradFunc = CreateGradientFunction ((cairo_gradient_pattern_t*) rpat);
 

src/cairo-analysis-surface.c
@@ -96,10 +96,10 @@ _cairo_analysis_surface_merge_status (cairo_int_status_t status_a,
 
 static cairo_int_status_t
 _analyze_meta_surface_pattern (cairo_analysis_surface_t	*surface,
-			       cairo_pattern_t		*pattern)
+			       const cairo_pattern_t *pattern)
 {
     cairo_surface_t *analysis = &surface->base;
-    cairo_surface_pattern_t *surface_pattern;
+    const cairo_surface_pattern_t *surface_pattern;
     cairo_status_t status;
     cairo_bool_t old_has_ctm;
     cairo_matrix_t old_ctm, p2d;
@@ -109,7 +109,7 @@ _analyze_meta_surface_pattern (cairo_analysis_surface_t	*surface,
     int old_height;
 
     assert (pattern->type == CAIRO_PATTERN_TYPE_SURFACE);
-    surface_pattern = (cairo_surface_pattern_t *) pattern;
+    surface_pattern = (const cairo_surface_pattern_t *) pattern;
     assert (_cairo_surface_is_meta (surface_pattern->surface));
 
     old_width = surface->width;
@@ -321,8 +321,8 @@ _cairo_analysis_surface_get_extents (void			*abstract_surface,
 
 static cairo_int_status_t
 _cairo_analysis_surface_paint (void			*abstract_surface,
-			      cairo_operator_t		op,
-			      cairo_pattern_t		*source)
+			       cairo_operator_t		op,
+			       const cairo_pattern_t		*source)
 {
     cairo_analysis_surface_t *surface = abstract_surface;
     cairo_status_t	     status, backend_status;
@@ -362,8 +362,8 @@ _cairo_analysis_surface_paint (void			*abstract_surface,
 static cairo_int_status_t
 _cairo_analysis_surface_mask (void		*abstract_surface,
 			      cairo_operator_t	 op,
-			      cairo_pattern_t	*source,
-			      cairo_pattern_t	*mask)
+			      const cairo_pattern_t	*source,
+			      const cairo_pattern_t	*mask)
 {
     cairo_analysis_surface_t *surface = abstract_surface;
     cairo_int_status_t	      status, backend_status;
@@ -381,7 +381,7 @@ _cairo_analysis_surface_mask (void		*abstract_surface,
 	cairo_int_status_t backend_mask_status = CAIRO_STATUS_SUCCESS;
 
 	if (source->type == CAIRO_PATTERN_TYPE_SURFACE) {
-	    cairo_surface_pattern_t *surface_pattern = (cairo_surface_pattern_t *) source;
+	    const cairo_surface_pattern_t *surface_pattern = (const cairo_surface_pattern_t *) source;
 	    if (_cairo_surface_is_meta (surface_pattern->surface)) {
 		backend_source_status =
 		    _analyze_meta_surface_pattern (surface, source);
@@ -411,6 +411,7 @@ _cairo_analysis_surface_mask (void		*abstract_surface,
 
     if (_cairo_operator_bounded_by_source (op)) {
 	cairo_rectangle_int_t source_extents;
+
 	status = _cairo_pattern_get_extents (source, &source_extents);
 	if (status)
 	    return status;
@@ -438,7 +439,7 @@ _cairo_analysis_surface_mask (void		*abstract_surface,
 static cairo_int_status_t
 _cairo_analysis_surface_stroke (void			*abstract_surface,
 				cairo_operator_t	 op,
-				cairo_pattern_t		*source,
+				const cairo_pattern_t	*source,
 				cairo_path_fixed_t	*path,
 				cairo_stroke_style_t	*style,
 				cairo_matrix_t		*ctm,
@@ -510,7 +511,7 @@ _cairo_analysis_surface_stroke (void			*abstract_surface,
 static cairo_int_status_t
 _cairo_analysis_surface_fill (void			*abstract_surface,
 			      cairo_operator_t		 op,
-			      cairo_pattern_t		*source,
+			      const cairo_pattern_t	*source,
 			      cairo_path_fixed_t	*path,
 			      cairo_fill_rule_t		 fill_rule,
 			      double			 tolerance,
@@ -578,7 +579,7 @@ _cairo_analysis_surface_fill (void			*abstract_surface,
 static cairo_int_status_t
 _cairo_analysis_surface_show_glyphs (void		  *abstract_surface,
 				     cairo_operator_t	   op,
-				     cairo_pattern_t	  *source,
+				     const cairo_pattern_t *source,
 				     cairo_glyph_t	  *glyphs,
 				     int		   num_glyphs,
 				     cairo_scaled_font_t  *scaled_font,
@@ -653,7 +654,7 @@ _cairo_analysis_surface_has_show_text_glyphs (void *abstract_surface)
 static cairo_int_status_t
 _cairo_analysis_surface_show_text_glyphs (void			    *abstract_surface,
 					  cairo_operator_t	     op,
-					  cairo_pattern_t	    *source,
+					  const cairo_pattern_t	    *source,
 					  const char		    *utf8,
 					  int			     utf8_len,
 					  cairo_glyph_t		    *glyphs,
@@ -897,18 +898,18 @@ typedef cairo_int_status_t
 typedef cairo_int_status_t
 (*_paint_func)			(void			*surface,
 			         cairo_operator_t	 op,
-				 cairo_pattern_t	*source);
+				 const cairo_pattern_t	*source);
 
 typedef cairo_int_status_t
 (*_mask_func)			(void			*surface,
 			         cairo_operator_t	 op,
-				 cairo_pattern_t	*source,
-				 cairo_pattern_t	*mask);
+				 const cairo_pattern_t	*source,
+				 const cairo_pattern_t	*mask);
 
 typedef cairo_int_status_t
 (*_stroke_func)			(void			*surface,
 			         cairo_operator_t	 op,
-				 cairo_pattern_t	*source,
+				 const cairo_pattern_t	*source,
 				 cairo_path_fixed_t	*path,
 				 cairo_stroke_style_t	*style,
 				 cairo_matrix_t		*ctm,
@@ -919,7 +920,7 @@ typedef cairo_int_status_t
 typedef cairo_int_status_t
 (*_fill_func)			(void			*surface,
 			         cairo_operator_t	 op,
-				 cairo_pattern_t	*source,
+				 const cairo_pattern_t	*source,
 				 cairo_path_fixed_t	*path,
 				 cairo_fill_rule_t	 fill_rule,
 				 double			 tolerance,
@@ -928,7 +929,7 @@ typedef cairo_int_status_t
 typedef cairo_int_status_t
 (*_show_glyphs_func)		(void			*surface,
 			         cairo_operator_t	 op,
-				 cairo_pattern_t	*source,
+				 const cairo_pattern_t	*source,
 				 cairo_glyph_t		*glyphs,
 				 int			 num_glyphs,
 				 cairo_scaled_font_t	*scaled_font,

src/cairo-directfb-surface.c
@@ -702,8 +702,8 @@ _cairo_directfb_surface_clone_similar (void             *abstract_surface,
 #if DFB_COMPOSITE || DFB_COMPOSITE_TRAPEZOIDS
 static cairo_int_status_t
 _directfb_prepare_composite (cairo_directfb_surface_t    *dst,
-                             cairo_pattern_t             *src_pattern,
-                             cairo_pattern_t             *mask_pattern,
+                             const cairo_pattern_t       *src_pattern,
+                             const cairo_pattern_t       *mask_pattern,
                              cairo_operator_t             op,
                              int *src_x,             int *src_y,
                              int *mask_x,            int *mask_y,
@@ -732,7 +732,7 @@ _directfb_prepare_composite (cairo_directfb_surface_t    *dst,
 
 		return CAIRO_INT_STATUS_UNSUPPORTED;
 	if (mask_pattern->type != CAIRO_PATTERN_TYPE_SOLID) {
-	    cairo_pattern_t *tmp;
+	    const cairo_pattern_t *tmp;
 	    int              tmp_x, tmp_y;
 
 	    if (src_pattern->type != CAIRO_PATTERN_TYPE_SOLID ||
@@ -760,11 +760,6 @@ _directfb_prepare_composite (cairo_directfb_surface_t    *dst,
     } else {
 	color = _cairo_stock_color (CAIRO_STOCK_WHITE);
     }
-
-    /* XXX DirectFB currently does not support filtering, so force NEAREST
-     * in order to hit optimisations inside core.
-    */
-    src_pattern->filter = CAIRO_FILTER_NEAREST;
 
     status = _cairo_pattern_acquire_surface (src_pattern, &dst->base,
 					     *src_x, *src_y, width, height,
@@ -842,7 +837,7 @@ _directfb_prepare_composite (cairo_directfb_surface_t    *dst,
 
 static void
 _directfb_finish_composite (cairo_directfb_surface_t   *dst,
-                            cairo_pattern_t            *src_pattern,
+                            const cairo_pattern_t      *src_pattern,
                             cairo_surface_t            *src,
                             cairo_surface_attributes_t *src_attr)
 {
@@ -892,8 +887,8 @@ _directfb_categorize_operation (cairo_surface_attributes_t *src_attr)
 
 static cairo_int_status_t
 _cairo_directfb_surface_composite (cairo_operator_t  op,
-                                   cairo_pattern_t  *src_pattern,
-                                   cairo_pattern_t  *mask_pattern,
+                                   const cairo_pattern_t  *src_pattern,
+                                   const cairo_pattern_t  *mask_pattern,
                                    void             *abstract_dst,
                                    int  src_x,  int  src_y,
                                    int  mask_x, int  mask_y,
@@ -1160,7 +1155,7 @@ _cairo_directfb_surface_fill_rectangles (void                  *abstract_surface
 #if DFB_COMPOSITE_TRAPEZOIDS
 static cairo_int_status_t
 _cairo_directfb_surface_composite_trapezoids (cairo_operator_t   op,
-                                              cairo_pattern_t   *pattern,
+                                              const cairo_pattern_t   *pattern,
                                               void              *abstract_dst,
                                               cairo_antialias_t  antialias,
                                               int  src_x,   int  src_y,
@@ -1686,13 +1681,13 @@ _cairo_directfb_surface_scaled_glyph_fini (cairo_scaled_glyph_t *scaled_glyph,
 }
 
 static cairo_int_status_t
-_cairo_directfb_surface_show_glyphs (void                *abstract_dst,
-                                     cairo_operator_t     op,
-                                     cairo_pattern_t     *pattern,
-                                     cairo_glyph_t       *glyphs,
-                                     int                  num_glyphs,
-                                     cairo_scaled_font_t *scaled_font,
-				     int		 *remaining_glyphs)
+_cairo_directfb_surface_show_glyphs (void		    *abstract_dst,
+                                     cairo_operator_t	     op,
+                                     const cairo_pattern_t  *pattern,
+                                     cairo_glyph_t	    *glyphs,
+                                     int		     num_glyphs,
+                                     cairo_scaled_font_t    *scaled_font,
+				     int		    *remaining_glyphs)
 {
     cairo_directfb_surface_t    *dst = abstract_dst;
     cairo_directfb_font_cache_t *cache;

src/cairo-glitz-surface.c
@@ -718,7 +718,7 @@ typedef struct _cairo_glitz_surface_attributes {
 } cairo_glitz_surface_attributes_t;
 
 static cairo_int_status_t
-_cairo_glitz_pattern_acquire_surface (cairo_pattern_t	              *pattern,
+_cairo_glitz_pattern_acquire_surface (const cairo_pattern_t	       *pattern,
 				      cairo_glitz_surface_t	       *dst,
 				      int			       x,
 				      int			       y,
@@ -937,7 +937,7 @@ _cairo_glitz_pattern_acquire_surface (cairo_pattern_t	              *pattern,
 }
 
 static void
-_cairo_glitz_pattern_release_surface (cairo_pattern_t		      *pattern,
+_cairo_glitz_pattern_release_surface (const cairo_pattern_t	      *pattern,
 				      cairo_glitz_surface_t	      *surface,
 				      cairo_glitz_surface_attributes_t *attr)
 {
@@ -948,8 +948,8 @@ _cairo_glitz_pattern_release_surface (cairo_pattern_t		      *pattern,
 }
 
 static cairo_int_status_t
-_cairo_glitz_pattern_acquire_surfaces (cairo_pattern_t	                *src,
-				       cairo_pattern_t	                *mask,
+_cairo_glitz_pattern_acquire_surfaces (const cairo_pattern_t	       *src,
+				       const cairo_pattern_t	       *mask,
 				       cairo_glitz_surface_t	        *dst,
 				       int			        src_x,
 				       int			        src_y,
@@ -985,37 +985,31 @@ _cairo_glitz_pattern_acquire_surfaces (cairo_pattern_t	                *src,
 	_cairo_pattern_init_solid (&tmp, &combined, CAIRO_CONTENT_COLOR_ALPHA);
 
 	mask = NULL;
-    } else {
-	status = _cairo_pattern_init_copy (&tmp.base, src);
-	if (status)
-	    return status;
+	src = &tmp.base;
     }
 
-    status = _cairo_glitz_pattern_acquire_surface (&tmp.base, dst,
+    status = _cairo_glitz_pattern_acquire_surface (src, dst,
 						   src_x, src_y,
 						   width, height,
 						   src_out, sattr);
 
-    _cairo_pattern_fini (&tmp.base);
+    if (src == &tmp.base)
+	_cairo_pattern_fini (&tmp.base);
 
     if (status)
 	return status;
 
     if (mask)
     {
-	status = _cairo_pattern_init_copy (&tmp.base, mask);
-	if (status)
-	    return status;
-
-	status = _cairo_glitz_pattern_acquire_surface (&tmp.base, dst,
+	status = _cairo_glitz_pattern_acquire_surface (mask, dst,
 						       mask_x, mask_y,
 						       width, height,
 						       mask_out, mattr);
 
-	if (status)
-	    _cairo_glitz_pattern_release_surface (&tmp.base, *src_out, sattr);
-
-	_cairo_pattern_fini (&tmp.base);
+	if (status) {
+	    /* XXX src == &tmp.base -> invalid (currently inconsequential) */
+	    _cairo_glitz_pattern_release_surface (src, *src_out, sattr);
+	}
 
 	return status;
     }
@@ -1039,8 +1033,8 @@ _cairo_glitz_surface_set_attributes (cairo_glitz_surface_t	      *surface,
 
 static cairo_int_status_t
 _cairo_glitz_surface_composite (cairo_operator_t op,
-				cairo_pattern_t  *src_pattern,
-				cairo_pattern_t  *mask_pattern,
+				const cairo_pattern_t *src_pattern,
+				const cairo_pattern_t *mask_pattern,
 				void		 *abstract_dst,
 				int		 src_x,
 				int		 src_y,
@@ -1197,7 +1191,7 @@ _cairo_glitz_surface_fill_rectangles (void		      *abstract_dst,
 
 static cairo_int_status_t
 _cairo_glitz_surface_composite_trapezoids (cairo_operator_t  op,
-					   cairo_pattern_t   *pattern,
+					   const cairo_pattern_t *pattern,
 					   void		     *abstract_dst,
 					   cairo_antialias_t antialias,
 					   int		     src_x,
@@ -1210,7 +1204,7 @@ _cairo_glitz_surface_composite_trapezoids (cairo_operator_t  op,
 					   int		     n_traps)
 {
     cairo_pattern_union_t	     tmp_src_pattern;
-    cairo_pattern_t		     *src_pattern;
+    const cairo_pattern_t	    *src_pattern;
     cairo_glitz_surface_attributes_t attributes;
     cairo_glitz_surface_t	     *dst = abstract_dst;
     cairo_glitz_surface_t	     *src;
@@ -2101,7 +2095,7 @@ _cairo_glitz_surface_add_glyph (cairo_glitz_surface_t *surface,
 static cairo_int_status_t
 _cairo_glitz_surface_old_show_glyphs (cairo_scaled_font_t *scaled_font,
 				      cairo_operator_t     op,
-				      cairo_pattern_t     *pattern,
+				      const cairo_pattern_t *pattern,
 				      void		  *abstract_surface,
 				      int		   src_x,
 				      int		   src_y,

commit 57551a91168a68399add9e5c324fba03961b7747

[quartz] Fix fallout caused by const cairo_pattern_t *
794460186459981cd43ca38f3eba07e3f6bf8908 broke the quartz backend.
Fix it by adding const to the appropriate parameters in the function
declarations and copying the pattern when needed.

src/cairo-quartz-surface.c
@@ -631,7 +631,7 @@ static void
 ComputeGradientValue (void *info, const float *in, float *out)
 {
     double fdist = *in;
-    cairo_gradient_pattern_t *grad = (cairo_gradient_pattern_t*) info;
+    const cairo_gradient_pattern_t *grad = (cairo_gradient_pattern_t*) info;
     unsigned int i;
 
     /* Put fdist back in the 0.0..1.0 range if we're doing
@@ -688,7 +688,7 @@ CreateGradientFunction (cairo_gradient_pattern_t *gpat)
 	0, ComputeGradientValue, (CGFunctionReleaseInfoCallback) cairo_pattern_destroy
     };
 
-    return CGFunctionCreate (_cairo_pattern_create_copy (&gpat->base),
+    return CGFunctionCreate (cairo_pattern_reference (&gpat->base),
 			     1,
 			     input_value_range,
 			     4,
@@ -766,7 +766,7 @@ CreateRepeatingGradientFunction (cairo_quartz_surface_t *surface,
     input_value_range[0] = 0.0 - 1.0 * rep_start;
     input_value_range[1] = 1.0 + 1.0 * rep_end;
 
-    return CGFunctionCreate (_cairo_pattern_create_copy (&gpat->base),
+    return CGFunctionCreate (cairo_pattern_reference (&gpat->base),
 			     1,
 			     input_value_range,
 			     4,
@@ -904,7 +904,7 @@ SurfacePatternReleaseInfoFunc (void *ainfo)
 
 static cairo_int_status_t
 _cairo_quartz_cairo_repeating_surface_pattern_to_quartz (cairo_quartz_surface_t *dest,
-							 cairo_pattern_t *apattern,
+							 const cairo_pattern_t *apattern,
 							 CGPatternRef *cgpat)
 {
     cairo_surface_pattern_t *spattern;
@@ -1174,7 +1174,7 @@ _cairo_quartz_setup_radial_source (cairo_quartz_surface_t *surface,
 
 static cairo_quartz_action_t
 _cairo_quartz_setup_source (cairo_quartz_surface_t *surface,
-			    cairo_pattern_t *source)
+			    const cairo_pattern_t *source)
 {
     assert (!(surface->sourceImage || surface->sourceShading || surface->sourcePattern));
 
@@ -1320,7 +1320,7 @@ _cairo_quartz_setup_source (cairo_quartz_surface_t *surface,
 
 static void
 _cairo_quartz_teardown_source (cairo_quartz_surface_t *surface,
-				cairo_pattern_t *source)
+			       const cairo_pattern_t *source)
 {
     CGContextSetInterpolationQuality (surface->cgContext, surface->oldInterpolationQuality);
 
@@ -1667,7 +1667,7 @@ _cairo_quartz_surface_get_extents (void *abstract_surface,
 static cairo_int_status_t
 _cairo_quartz_surface_paint (void *abstract_surface,
 			     cairo_operator_t op,
-			     cairo_pattern_t *source,
+			     const cairo_pattern_t *source,
 			     cairo_rectangle_int_t *extents)
 {
     cairo_quartz_surface_t *surface = (cairo_quartz_surface_t *) abstract_surface;
@@ -1719,7 +1719,7 @@ _cairo_quartz_surface_paint (void *abstract_surface,
 static cairo_int_status_t
 _cairo_quartz_surface_fill (void *abstract_surface,
 			     cairo_operator_t op,
-			     cairo_pattern_t *source,
+			     const cairo_pattern_t *source,
 			     cairo_path_fixed_t *path,
 			     cairo_fill_rule_t fill_rule,
 			     double tolerance,
@@ -1825,7 +1825,7 @@ _cairo_quartz_surface_fill (void *abstract_surface,
 static cairo_int_status_t
 _cairo_quartz_surface_stroke (void *abstract_surface,
 			      cairo_operator_t op,
-			      cairo_pattern_t *source,
+			      const cairo_pattern_t *source,
 			      cairo_path_fixed_t *path,
 			      cairo_stroke_style_t *style,
 			      cairo_matrix_t *ctm,
@@ -1972,7 +1972,7 @@ _cairo_quartz_surface_stroke (void *abstract_surface,
 static cairo_int_status_t
 _cairo_quartz_surface_show_glyphs (void *abstract_surface,
 				   cairo_operator_t op,
-				   cairo_pattern_t *source,
+				   const cairo_pattern_t *source,
 				   cairo_glyph_t *glyphs,
 				   int num_glyphs,
 				   cairo_scaled_font_t *scaled_font,
@@ -2182,8 +2182,8 @@ BAIL:
 static cairo_int_status_t
 _cairo_quartz_surface_mask_with_surface (cairo_quartz_surface_t *surface,
                                          cairo_operator_t op,
-                                         cairo_pattern_t *source,
-                                         cairo_surface_pattern_t *mask)
+                                         const cairo_pattern_t *source,
+                                         const cairo_surface_pattern_t *mask)
 {
     cairo_rectangle_int_t extents;
     CGRect rect;
@@ -2247,8 +2247,8 @@ _cairo_quartz_surface_mask_with_surface (cairo_quartz_surface_t *surface,
 static cairo_int_status_t
 _cairo_quartz_surface_mask_with_generic (cairo_quartz_surface_t *surface,
 					 cairo_operator_t op,
-					 cairo_pattern_t *source,
-					 cairo_pattern_t *mask)
+					 const cairo_pattern_t *source,
+					 const cairo_pattern_t *mask)
 {
     int width = surface->extents.width - surface->extents.x;
     int height = surface->extents.height - surface->extents.y;
@@ -2257,6 +2257,7 @@ _cairo_quartz_surface_mask_with_generic (cairo_quartz_surface_t *surface,
     cairo_t *gradient_surf_cr = NULL;
 
     cairo_surface_pattern_t surface_pattern;
+    cairo_pattern_t *mask_copy = NULL;
     cairo_int_status_t status;
 
     /* Render the gradient to a surface */
@@ -2264,11 +2265,17 @@ _cairo_quartz_surface_mask_with_generic (cairo_quartz_surface_t *surface,
 						 width,
 						 height);
     gradient_surf_cr = cairo_create(gradient_surf);
-    cairo_set_source (gradient_surf_cr, mask);
+
+    /* make a copy of the pattern because because cairo_set_source doesn't take
+     * a 'const cairo_pattern_t *' */
+    _cairo_pattern_create_copy (&mask_copy, mask);
+
+    cairo_set_source (gradient_surf_cr, mask_copy);
     cairo_set_operator (gradient_surf_cr, CAIRO_OPERATOR_SOURCE);
     cairo_paint (gradient_surf_cr);
     status = cairo_status (gradient_surf_cr);
     cairo_destroy (gradient_surf_cr);
+    cairo_pattern_destroy (mask_copy);
 
     if (status)
 	goto BAIL;
@@ -2289,8 +2296,8 @@ _cairo_quartz_surface_mask_with_generic (cairo_quartz_surface_t *surface,
 static cairo_int_status_t
 _cairo_quartz_surface_mask (void *abstract_surface,
 			    cairo_operator_t op,
-			    cairo_pattern_t *source,
-			    cairo_pattern_t *mask,
+			    const cairo_pattern_t *source,
+			    const cairo_pattern_t *mask,
 			    cairo_rectangle_int_t *extents)
 {
     cairo_quartz_surface_t *surface = (cairo_quartz_surface_t *) abstract_surface;