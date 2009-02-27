b71b019fe50a9188ddbecd1945606da8ba3bad53
Chris Wilson <chris@chris-wilson.co.uk>
2009-02-27 16:32:21 +0000

    [in-fill] Treat on-edge queries as inside.
    Jeff Muizelaar noted that the treatment of edges differed with firefox's
    canvas definition, which considers a point on any edge as inside. The
    current implementation has a similar definition to that of flash, for
    which the top and right edges are outside. Arguably, firefox has the more
    intuitive definition here...

--- src/cairo-path-in-fill.c
+++ src/cairo-path-in-fill.c
@@ -41,6 +41,7 @@ typedef struct cairo_in_fill {
     int winding;
 
     cairo_fixed_t x, y;
+    cairo_bool_t on_edge;
 
     cairo_bool_t has_current_point;
     cairo_point_t current_point;
@@ -58,6 +59,7 @@ _cairo_in_fill_init (cairo_in_fill_t	*in_fill,
 
     in_fill->x = _cairo_fixed_from_double (x);
     in_fill->y = _cairo_fixed_from_double (y);
+    in_fill->on_edge = FALSE;
 
     in_fill->has_current_point = FALSE;
     in_fill->current_point.x = 0;
@@ -103,6 +105,9 @@ _cairo_in_fill_add_edge (cairo_in_fill_t *in_fill,
 {
     int dir;
 
+    if (in_fill->on_edge)
+	return;
+
     /* count the number of edge crossing to -∞ */
 
     dir = 1;
@@ -114,6 +119,18 @@ _cairo_in_fill_add_edge (cairo_in_fill_t *in_fill,
 	p2 = tmp;
 
 	dir = -1;
+    }
+
+    /* First check whether the query is on an edge */
+    if ((p1->x == in_fill->x && p1->x == in_fill->y) ||
+	(p2->x == in_fill->x && p2->x == in_fill->y) ||
+	(! (p2->y < in_fill->y || p1->y > in_fill->y) &&
+	 ! (p1->x > in_fill->x && p2->x > in_fill->x) &&
+	 ! (p1->x < in_fill->x && p2->x < in_fill->x) &&
+	 edge_compare_for_y_against_x (p1, p2, in_fill->y, in_fill->x) == 0))
+    {
+	in_fill->on_edge = TRUE;
+	return;
     }
 
     /* edge is entirely above or below, note the shortening rule */
@@ -250,7 +267,9 @@ _cairo_path_fixed_in_fill (cairo_path_fixed_t	*path,
 
     _cairo_in_fill_close_path (&in_fill);
 
-    switch (fill_rule) {
+    if (in_fill.on_edge) {
+	*is_inside = TRUE;
+    } else switch (fill_rule) {
     case CAIRO_FILL_RULE_EVEN_ODD:
 	*is_inside = in_fill.winding & 1;
 	break;
