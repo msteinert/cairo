7651227d3dfe4ebf40e95a72106957a9ec51dd3e
M Joonas Pihlaja <jpihlaja@cc.helsinki.fi>
2009-06-19 02:03:39 +0300

    Revert "[test] Make the xlib-fallback use the image refs."
    This reverts commit 903b39c30448d62e2cbf9d075c5256a333bd5d8f.
    
    The xlib-fallback actually renders mostly via traps at the moment,
    so staying with xlib refs is the right thing to do.

--- boilerplate/cairo-boilerplate.c
+++ boilerplate/cairo-boilerplate.c
@@ -574,7 +574,7 @@ static const cairo_boilerplate_target_t targets[] =
     /* This is a fallback surface which uses xlib fallbacks instead of
      * the Render extension. */
     {
-	"xlib-fallback", "image", NULL,
+	"xlib-fallback", "xlib", NULL,
 	CAIRO_SURFACE_TYPE_XLIB, CAIRO_CONTENT_COLOR, 1,
 	_cairo_boilerplate_xlib_fallback_create_surface, NULL,
 	NULL,
