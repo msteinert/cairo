4152cd90e25ffa9d2a3683c790bd2891a58c52de
M Joonas Pihlaja <jpihlaja@cc.helsinki.fi>
2009-09-14 14:29:01 +0300

    [autoconf] Comment on the SIZEOF_VOID__ -> SIZEOF_VOID_P workaround.
    Explain where the weird SIZEOF_VOID__ comes from.

--- src/cairo-atomic-private.h
+++ src/cairo-atomic-private.h
@@ -43,6 +43,8 @@
 #include "config.h"
 #endif
 
+/* The autoconf on OpenBSD 4.5 produces the malformed constant name
+ * SIZEOF_VOID__ rather than SIZEOF_VOID_P.  Work around that here. */
 #if !defined(SIZEOF_VOID_P) && defined(SIZEOF_VOID__)
 # define SIZEOF_VOID_P SIZEOF_VOID__
 #endif
