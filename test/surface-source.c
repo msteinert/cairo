9948b5180aa4312b3184d9ba46e9ad6f0d8e700c
Chris Wilson <chris@chris-wilson.co.uk>
2009-09-22 18:20:03 +0100

    [test] Revamp surface-source
    Include a translucent region in the source that exercises the xlib bug
    that I'm trying to fix. Hmm.

--- test/surface-source.c
+++ test/surface-source.c
@@ -34,16 +34,22 @@ static cairo_surface_t *create_source_surface (int size);
  */
 #define SOURCE_SIZE 2000
 #define INTER_SIZE 512
-#define SIZE 90
+#define SIZE 96
 
 static void
 draw_pattern (cairo_surface_t **surface_inout, int surface_size)
 {
     cairo_t *cr;
+    int mid = surface_size/2;
 
     cr = cairo_create (*surface_inout);
     cairo_surface_destroy (*surface_inout);
 
+    cairo_rectangle (cr, 0, 0, surface_size, surface_size);
+    cairo_rectangle (cr, mid - SIZE/4, mid + SIZE/4, SIZE/2, -SIZE/2);
+    cairo_clip (cr);
+
+    /* outside squares -> opaque */
     cairo_set_source_rgb (cr, 1, 1, 1);
     cairo_rectangle (cr,
 		     0, 0,
@@ -64,6 +70,33 @@ draw_pattern (cairo_surface_t **surface_inout, int surface_size)
 		     surface_size / 2, surface_size / 2,
 		     surface_size / 2, surface_size / 2);
     cairo_fill (cr);
+
+    cairo_reset_clip (cr);
+    cairo_rectangle (cr, mid - SIZE/4, mid - SIZE/4, SIZE/2, SIZE/2);
+    cairo_clip (cr);
+
+    /* inside squares -> translucent */
+    cairo_set_source_rgba (cr, 0, 0, 1, .5);
+    cairo_rectangle (cr,
+		     0, 0,
+		     surface_size / 2, surface_size / 2);
+    cairo_fill (cr);
+    cairo_set_source_rgba (cr, 0, 1, 0, .5);
+    cairo_rectangle (cr,
+		     surface_size / 2, 0,
+		     surface_size / 2, surface_size / 2);
+    cairo_fill (cr);
+    cairo_set_source_rgba (cr, 1, 0, 0, .5);
+    cairo_rectangle (cr,
+		     0, surface_size / 2,
+		     surface_size / 2, surface_size / 2);
+    cairo_fill (cr);
+    cairo_set_source_rgba (cr, 1, 1, 1, .5);
+    cairo_rectangle (cr,
+		     surface_size / 2, surface_size / 2,
+		     surface_size / 2, surface_size / 2);
+    cairo_fill (cr);
+
 
     *surface_inout = cairo_surface_reference (cairo_get_target (cr));
     cairo_destroy (cr);
@@ -101,7 +134,7 @@ draw (cairo_t *cr, int width, int height)
 			      (width - INTER_SIZE)/2,
 			      (height - INTER_SIZE)/2);
     cairo_destroy (cr2);
-    cairo_rectangle (cr, 15, 15, 60, 60);
+    cairo_rectangle (cr, 16, 16, 64, 64);
     cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
     cairo_fill (cr);
 
