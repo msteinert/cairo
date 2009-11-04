a9f37953c71e16f65122097462e51b035783a965
Benjamin Otte <otte@gnome.org>
2009-11-04 13:59:58 +0100

    [xlib] Use correct number when checking if points are out of range

--- src/cairo-xlib-surface.c
+++ src/cairo-xlib-surface.c
@@ -2506,14 +2506,14 @@ static cairo_bool_t
 _line_exceeds_16_16 (const cairo_line_t *line)
 {
     return
-	line->p1.x < CAIRO_FIXED_16_16_MIN ||
-	line->p1.x > CAIRO_FIXED_16_16_MAX ||
-	line->p2.x < CAIRO_FIXED_16_16_MIN ||
-	line->p2.x > CAIRO_FIXED_16_16_MAX ||
-	line->p1.y < CAIRO_FIXED_16_16_MIN ||
-	line->p1.y > CAIRO_FIXED_16_16_MAX ||
-	line->p2.y < CAIRO_FIXED_16_16_MIN ||
-	line->p2.y > CAIRO_FIXED_16_16_MAX;
+	line->p1.x < _cairo_fixed_from_int (CAIRO_FIXED_16_16_MIN) ||
+	line->p1.x > _cairo_fixed_from_int (CAIRO_FIXED_16_16_MAX) ||
+	line->p2.x < _cairo_fixed_from_int (CAIRO_FIXED_16_16_MIN) ||
+	line->p2.x > _cairo_fixed_from_int (CAIRO_FIXED_16_16_MAX) ||
+	line->p1.y < _cairo_fixed_from_int (CAIRO_FIXED_16_16_MIN) ||
+	line->p1.y > _cairo_fixed_from_int (CAIRO_FIXED_16_16_MAX) ||
+	line->p2.y < _cairo_fixed_from_int (CAIRO_FIXED_16_16_MIN) ||
+	line->p2.y > _cairo_fixed_from_int (CAIRO_FIXED_16_16_MAX);
 }
 
 static void
