msteinert/cairo

commit 2d790daa957471670f4ae0d3b22da89e4ee7111f

[pattern] Use a solid pattern for a uniform gradient.
If each color stop in a gradient is identical, replace the gradient
surface with a simple solid surface. As seen in the wild.

src/cairo-pattern.c
@@ -2073,19 +2073,15 @@ _cairo_pattern_acquire_surface (const cairo_pattern_t	   *pattern,
 
 	    if (src->n_stops)
 	    {
-		cairo_color_t color;
-
-		_cairo_color_init_rgba (&color,
-					src->stops->color.red,
-					src->stops->color.green,
-					src->stops->color.blue,
-					src->stops->color.alpha);
-
-		_cairo_pattern_init_solid (&solid, &color, CAIRO_CONTENT_COLOR_ALPHA);
+		_cairo_pattern_init_solid (&solid,
+					   &src->stops->color,
+					   CAIRO_CONTENT_COLOR_ALPHA);
 	    }
 	    else
 	    {
-		_cairo_pattern_init_solid (&solid, CAIRO_COLOR_TRANSPARENT, CAIRO_CONTENT_ALPHA);
+		_cairo_pattern_init_solid (&solid,
+					   CAIRO_COLOR_TRANSPARENT,
+					   CAIRO_CONTENT_ALPHA);
 	    }
 
 	    status = _cairo_pattern_acquire_surface_for_solid (&solid, dst,
@@ -2096,11 +2092,39 @@ _cairo_pattern_acquire_surface (const cairo_pattern_t	   *pattern,
 	}
 	else
 	{
-	    status = _cairo_pattern_acquire_surface_for_gradient (src, dst,
-								  x, y,
-								  width, height,
-								  surface_out,
-								  attributes);
+	    unsigned int i;
+
+	    /* Is the gradient a uniform colour?
+	     * Happens more often than you would believe.
+	     */
+	    for (i = 1; i < src->n_stops; i++) {
+		if (! _cairo_color_equal (&src->stops[0].color,
+					  &src->stops[i].color))
+		{
+		    break;
+		}
+	    }
+	    if (i == src->n_stops) {
+		cairo_solid_pattern_t solid;
+
+		_cairo_pattern_init_solid (&solid,
+					   &src->stops->color,
+					   CAIRO_CONTENT_COLOR_ALPHA);
+
+		status =
+		    _cairo_pattern_acquire_surface_for_solid (&solid, dst,
+							      x, y,
+							      width, height,
+							      surface_out,
+							      attributes);
+	    } else {
+		status =
+		    _cairo_pattern_acquire_surface_for_gradient (src, dst,
+								 x, y,
+								 width, height,
+								 surface_out,
+								 attributes);
+	    }
 	}
     } break;
     case CAIRO_PATTERN_TYPE_SURFACE: {

commit 4e315d84723b27746a6a012ab7de429b2f2ef90b

pattern: Premultiply color-stop when converting to solid
Fixes: test/linear-uniform
       https://bugzilla.mozilla.org/show_bug.cgi?id=539165
       Bug 539165 - gradients with a single stop do not have their color
                    multiplied by the alpha.

As reported by Jeff Muizelaar, we regressed in 2d790daa as the
color-stops are not premultiplied and so could not be treated as an
ordinary cairo_color_t. Instead we have to create a intermediate
cairo_color_t from the original values in order for the
premultiplication to be performed.

src/cairo-gstate.c
@@ -821,6 +821,21 @@ _cairo_gstate_path_extents (cairo_gstate_t     *gstate,
 }
 
 static void
+_init_solid_for_color_stop (cairo_solid_pattern_t *solid,
+			    const cairo_color_t *color)
+{
+    cairo_color_t premult;
+
+    /* Color stops aren't premultiplied, so fix that here */
+    _cairo_color_init_rgba (&premult,
+			    color->red,
+			    color->green,
+			    color->blue,
+			    color->alpha);
+    _cairo_pattern_init_solid (solid, &premult, CAIRO_CONTENT_COLOR_ALPHA);
+}
+
+static void
 _cairo_gstate_copy_pattern (cairo_pattern_t *pattern,
 			    const cairo_pattern_t *original)
 {
@@ -841,9 +856,8 @@ _cairo_gstate_copy_pattern (cairo_pattern_t *pattern,
 	    /* fast path for gradients with less than 2 color stops */
 	    if (src->n_stops < 2) {
 		if (src->n_stops) {
-		    _cairo_pattern_init_solid ((cairo_solid_pattern_t *) pattern,
-					       &src->stops->color,
-					       CAIRO_CONTENT_COLOR_ALPHA);
+		    _init_solid_for_color_stop ((cairo_solid_pattern_t *) pattern,
+						&src->stops->color);
 		} else {
 		    _cairo_pattern_init_solid ((cairo_solid_pattern_t *) pattern,
 					       CAIRO_COLOR_TRANSPARENT,
@@ -865,10 +879,8 @@ _cairo_gstate_copy_pattern (cairo_pattern_t *pattern,
 		    }
 		}
 		if (i == src->n_stops) {
-		    _cairo_pattern_init_solid ((cairo_solid_pattern_t *) pattern,
-					       &src->stops->color,
-					       CAIRO_CONTENT_COLOR_ALPHA);
-
+		    _init_solid_for_color_stop ((cairo_solid_pattern_t *) pattern,
+						&src->stops->color);
 		    return;
 		}
 	    }

src/cairo-pattern.c
@@ -2206,6 +2206,21 @@ _cairo_pattern_acquire_surface_for_surface (const cairo_surface_pattern_t   *pat
     return status;
 }
 
+static void
+_init_solid_for_color_stop (cairo_solid_pattern_t *solid,
+			    const cairo_color_t *color)
+{
+    cairo_color_t premult;
+
+    /* Color stops aren't premultiplied, so fix that here */
+    _cairo_color_init_rgba (&premult,
+			    color->red,
+			    color->green,
+			    color->blue,
+			    color->alpha);
+    _cairo_pattern_init_solid (solid, &premult, CAIRO_CONTENT_COLOR_ALPHA);
+}
+
 /**
  * _cairo_pattern_acquire_surface:
  * @pattern: a #cairo_pattern_t
@@ -2257,19 +2272,16 @@ _cairo_pattern_acquire_surface (const cairo_pattern_t	   *pattern,
     case CAIRO_PATTERN_TYPE_RADIAL: {
 	cairo_gradient_pattern_t *src = (cairo_gradient_pattern_t *) pattern;
 
+	/* XXX The gradient->solid conversion code should now be redundant. */
+
 	/* fast path for gradients with less than 2 color stops */
 	if (src->n_stops < 2)
 	{
 	    cairo_solid_pattern_t solid;
 
-	    if (src->n_stops)
-	    {
-		_cairo_pattern_init_solid (&solid,
-					   &src->stops->color,
-					   CAIRO_CONTENT_COLOR_ALPHA);
-	    }
-	    else
-	    {
+	    if (src->n_stops) {
+		_init_solid_for_color_stop (&solid, &src->stops->color);
+	    } else {
 		_cairo_pattern_init_solid (&solid,
 					   CAIRO_COLOR_TRANSPARENT,
 					   CAIRO_CONTENT_ALPHA);
@@ -2298,9 +2310,7 @@ _cairo_pattern_acquire_surface (const cairo_pattern_t	   *pattern,
 	    if (i == src->n_stops) {
 		cairo_solid_pattern_t solid;
 
-		_cairo_pattern_init_solid (&solid,
-					   &src->stops->color,
-					   CAIRO_CONTENT_COLOR_ALPHA);
+		_init_solid_for_color_stop (&solid, &src->stops->color);
 
 		status =
 		    _cairo_pattern_acquire_surface_for_solid (&solid, dst,