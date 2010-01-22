6bfcf3ea55964fee1c9b73818c3bb7cdacbc4f82
Chris Wilson <chris@chris-wilson.co.uk>
2010-01-22 15:37:25 +0000

    matrix: Do not need to iteratively refine conversion for unity matrices.
    If the scale factors on the matrix are unity, then the conversion from a
    cairo_matrix_t to a pixman_matrix_t is exact (within numerical
    restrictions).

--- src/cairo-matrix.c
+++ src/cairo-matrix.c
@@ -915,7 +915,7 @@ _cairo_matrix_to_pixman_matrix (const cairo_matrix_t	*matrix,
          * that point.
 	 */
 
-	if (_cairo_matrix_is_translation (matrix))
+	if (_cairo_matrix_has_unity_scale (matrix))
 	    return;
 
         /* Note: If we can't invert the transformation, skip the adjustment. */
