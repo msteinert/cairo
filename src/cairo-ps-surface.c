f57cb45fa9274351b36f96eb932a931493b5db94
Chris Wilson <chris@chris-wilson.co.uk>
2010-01-22 22:18:35 +0000

    ps: Arguments to fill_extents() were reversed.

--- src/cairo-ps-surface.c
+++ src/cairo-ps-surface.c
@@ -3605,7 +3605,8 @@ _cairo_ps_surface_fill (void		*abstract_surface,
     /* use the more accurate extents */
     if (extents.is_bounded) {
 	_cairo_path_fixed_fill_extents (path,
-					tolerance, fill_rule,
+					fill_rule,
+					tolerance,
 					&extents.mask);
 
 	if (! _cairo_rectangle_intersect (&extents.bounded, &extents.mask))
