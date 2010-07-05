19da8e138d317c1770d8bbd21a49c657d954f126
Benjamin Otte <otte@redhat.com>
2010-07-05 22:40:54 +0200

    tee: Add proper error handling
    Caught by api-special-cases

--- src/cairo-tee-surface.c
+++ src/cairo-tee-surface.c
@@ -468,6 +468,11 @@ cairo_tee_surface_add (cairo_surface_t *abstract_surface,
 
     if (unlikely (abstract_surface->status))
 	return;
+    if (unlikely (abstract_surface->finished)) {
+	status = _cairo_surface_set_error (abstract_surface,
+					   _cairo_error (CAIRO_STATUS_SURFACE_FINISHED));
+	return;
+    }
 
     if (abstract_surface->backend != &cairo_tee_surface_backend) {
 	status = _cairo_surface_set_error (abstract_surface,
@@ -499,6 +504,14 @@ cairo_tee_surface_remove (cairo_surface_t *abstract_surface,
     cairo_surface_wrapper_t *slaves;
     int n, num_slaves;
     cairo_status_t status;
+
+    if (unlikely (abstract_surface->status))
+	return;
+    if (unlikely (abstract_surface->finished)) {
+	status = _cairo_surface_set_error (abstract_surface,
+					   _cairo_error (CAIRO_STATUS_SURFACE_FINISHED));
+	return;
+    }
 
     if (abstract_surface->backend != &cairo_tee_surface_backend) {
 	status = _cairo_surface_set_error (abstract_surface,
@@ -540,6 +553,8 @@ cairo_tee_surface_index (cairo_surface_t *abstract_surface,
 
     if (unlikely (abstract_surface->status))
 	return _cairo_surface_create_in_error (abstract_surface->status);
+    if (unlikely (abstract_surface->finished))
+	return _cairo_surface_create_in_error (_cairo_error (CAIRO_STATUS_SURFACE_FINISHED));
 
     if (abstract_surface->backend != &cairo_tee_surface_backend)
 	return _cairo_surface_create_in_error (_cairo_error (CAIRO_STATUS_SURFACE_TYPE_MISMATCH));
