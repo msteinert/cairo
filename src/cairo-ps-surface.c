f58ade7bace8c82d0ea6740f56d227696181d616
Chris Wilson <chris@chris-wilson.co.uk>
2011-07-19 21:14:08 +0100

    ps: Add HAS_FT_FONT guards for type1
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/cairo-ps-surface.c
+++ src/cairo-ps-surface.c
@@ -339,6 +339,8 @@ _cairo_ps_surface_emit_header (cairo_ps_surface_t *surface)
     }
 }
 
+#if CAIRO_HAS_FT_FONT
+
 static cairo_status_t
 _cairo_ps_surface_emit_type1_font_subset (cairo_ps_surface_t		*surface,
 					  cairo_scaled_font_subset_t	*font_subset)
@@ -371,6 +373,7 @@ _cairo_ps_surface_emit_type1_font_subset (cairo_ps_surface_t		*surface,
     return CAIRO_STATUS_SUCCESS;
 }
 
+
 static cairo_status_t
 _cairo_ps_surface_emit_type1_font_fallback (cairo_ps_surface_t		*surface,
                                             cairo_scaled_font_subset_t	*font_subset)
@@ -400,6 +403,23 @@ _cairo_ps_surface_emit_type1_font_fallback (cairo_ps_surface_t		*surface,
 
     return CAIRO_STATUS_SUCCESS;
 }
+#else
+
+static cairo_status_t
+_cairo_ps_surface_emit_type1_font_subset (cairo_ps_surface_t		*surface,
+					  cairo_scaled_font_subset_t	*font_subset)
+{
+    return CAIRO_INT_STATUS_UNSUPPORTED;
+}
+
+static cairo_status_t
+_cairo_ps_surface_emit_type1_font_fallback (cairo_ps_surface_t		*surface,
+                                            cairo_scaled_font_subset_t	*font_subset)
+{
+    return CAIRO_INT_STATUS_UNSUPPORTED;
+}
+
+#endif
 
 static cairo_status_t
 _cairo_ps_surface_emit_truetype_font_subset (cairo_ps_surface_t		*surface,
