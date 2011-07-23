abd8fcc833eedc916addbd688a40303e4c78affd
Chris Wilson <chris@chris-wilson.co.uk>
2011-07-23 14:11:12 +0100

    test: Expand partial-clip-text
    Test partial clipping along each edge.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- test/partial-clip-text.c
+++ test/partial-clip-text.c
@@ -26,25 +26,95 @@
 
 #include "cairo-test.h"
 
-static cairo_test_status_t
-draw (cairo_t *cr, int width, int height)
+#define HEIGHT 15
+#define WIDTH 40
+
+static void background (cairo_t *cr)
 {
      cairo_set_source_rgb( cr, 0, 0, 0 );
      cairo_paint (cr);
+}
 
-     cairo_rectangle (cr, 0, 0, 40, 5);
-     cairo_clip (cr);
-
+static void text (cairo_t *cr)
+{
      cairo_move_to (cr, 0, 12);
      cairo_set_source_rgb (cr, 1, 1, 1);
      cairo_show_text (cr, "CAIRO");
+}
+
+static cairo_test_status_t
+top (cairo_t *cr, int width, int height)
+{
+     background (cr);
+
+     cairo_rectangle (cr, 0, 0, WIDTH, 5);
+     cairo_clip (cr);
+
+     text (cr);
 
      return CAIRO_TEST_SUCCESS;
 }
 
-CAIRO_TEST (partial_clip_text,
+static cairo_test_status_t
+bottom (cairo_t *cr, int width, int height)
+{
+     background (cr);
+
+     cairo_rectangle (cr, 0, HEIGHT-5, WIDTH, 5);
+     cairo_clip (cr);
+
+     text (cr);
+
+     return CAIRO_TEST_SUCCESS;
+}
+
+static cairo_test_status_t
+left (cairo_t *cr, int width, int height)
+{
+     background (cr);
+
+     cairo_rectangle (cr, 0, 0, 10, HEIGHT);
+     cairo_clip (cr);
+
+     text (cr);
+
+     return CAIRO_TEST_SUCCESS;
+}
+
+static cairo_test_status_t
+right (cairo_t *cr, int width, int height)
+{
+     background (cr);
+
+     cairo_rectangle (cr, WIDTH-10, 0, 10, HEIGHT);
+     cairo_clip (cr);
+
+     text (cr);
+
+     return CAIRO_TEST_SUCCESS;
+}
+
+CAIRO_TEST (partial_clip_text_top,
 	    "Tests drawing text through a single, partial clip.",
 	    "clip, text", /* keywords */
 	    NULL, /* requirements */
-	    40, 15,
-	    NULL, draw)
+	    WIDTH, HEIGHT,
+	    NULL, top)
+CAIRO_TEST (partial_clip_text_bottom,
+	    "Tests drawing text through a single, partial clip.",
+	    "clip, text", /* keywords */
+	    NULL, /* requirements */
+	    WIDTH, HEIGHT,
+	    NULL, bottom)
+CAIRO_TEST (partial_clip_text_left,
+	    "Tests drawing text through a single, partial clip.",
+	    "clip, text", /* keywords */
+	    NULL, /* requirements */
+	    WIDTH, HEIGHT,
+	    NULL, left)
+CAIRO_TEST (partial_clip_text_right,
+	    "Tests drawing text through a single, partial clip.",
+	    "clip, text", /* keywords */
+	    NULL, /* requirements */
+	    WIDTH, HEIGHT,
+	    NULL, right)
