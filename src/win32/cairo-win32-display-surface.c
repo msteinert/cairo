83af31f0f31bee5570148f51d9371e848b716486
Chris Wilson <chris@chris-wilson.co.uk>
2012-02-15 19:48:07 +0000

    win32: Cast the surface to an image-surface to find its parent (compile fix)
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/win32/cairo-win32-display-surface.c
+++ src/win32/cairo-win32-display-surface.c
@@ -407,7 +407,7 @@ _cairo_win32_display_surface_finish (void *abstract_surface)
 
     if (surface->image) {
 	/* Unhook ourselves first to avoid the double-unref from the image */
-	surface->image->parent = NULL;
+	to_image_surface(surface->image)->parent = NULL;
 	cairo_surface_finish (surface->image);
 	cairo_surface_destroy (surface->image);
     }
