msteinert/cairo

commit 545f30856aac98199a49cf66c72dbcb66c1f3a4f

stroke: Convert the outlines into contour and then into a polygon
In step 1 of speeding up stroking, we introduce contours as a means for
tracking the connected edges around the stroke. By keeping track of
these chains, we can analyse the edges as we proceed and eliminate
redundant vertices speeding up rasterisation.

Coincidentally fixes line-width-tolerance (looks like a combination of
using spline tangent vectors and tolerance).

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/Makefile.sources
@@ -62,6 +62,7 @@ cairo_private = \
 	cairo-clip-private.h \
 	cairo-combsort-private.h \
 	cairo-compiler-private.h \
+	cairo-contour-private.h \
 	cairo-composite-rectangles-private.h \
 	cairo-default-context-private.h \
 	cairo-device-private.h \
@@ -132,6 +133,7 @@ cairo_sources = \
 	cairo-clip-surface.c \
 	cairo-color.c \
 	cairo-composite-rectangles.c \
+	cairo-contour.c \
 	cairo-debug.c \
 	cairo-default-context.c \
 	cairo-device.c \

src/cairo-contour-private.h
@@ -1,0 +1,159 @@
+/* cairo - a vector graphics library with display and print output
+ *
+ * Copyright © 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 1.1 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file COPYING-LGPL-2.1; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA
+ * You should have received a copy of the MPL along with this library
+ * in the file COPYING-MPL-1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ * The Original Code is the cairo graphics library.
+ *
+ * The Initial Developer of the Original Code is Intel Corporation
+ *
+ * Contributor(s):
+ *	Chris Wilson <chris@chris-wilson.co.uk>
+ */
+
+#ifndef CAIRO_CONTOUR_PRIVATE_H
+#define CAIRO_CONTOUR_PRIVATE_H
+
+#include "cairo-types-private.h"
+#include "cairo-compiler-private.h"
+#include "cairo-error-private.h"
+#include "cairo-list-private.h"
+
+#include <stdio.h>
+
+CAIRO_BEGIN_DECLS
+
+/* A contour is simply a closed chain of points that divide the infinite plane
+ * into inside and outside. Each contour is a simple polygon, that is it
+ * contains no holes or self-intersections, but maybe either concave or convex.
+ */
+
+struct _cairo_contour_chain {
+    cairo_point_t *points;
+    int num_points, size_points;
+    struct _cairo_contour_chain *next;
+};
+
+struct _cairo_contour_iter {
+    cairo_point_t *point;
+    cairo_contour_chain_t *chain;
+};
+
+struct _cairo_contour {
+    cairo_list_t next;
+    int direction;
+    cairo_contour_chain_t chain, *tail;
+
+    cairo_point_t embedded_points[64];
+};
+
+/* Initial definition of a shape is a set of contours (some representing holes) */
+struct _cairo_shape {
+    cairo_list_t contours;
+};
+
+typedef struct _cairo_shape cairo_shape_t;
+
+#if 0
+cairo_private cairo_status_t
+_cairo_shape_init_from_polygon (cairo_shape_t *shape,
+				const cairo_polygon_t *polygon);
+
+cairo_private cairo_status_t
+_cairo_shape_reduce (cairo_shape_t *shape, double tolerance);
+#endif
+
+cairo_private void
+_cairo_contour_init (cairo_contour_t *contour,
+		     int direction);
+
+cairo_private cairo_int_status_t
+__cairo_contour_add_point (cairo_contour_t *contour,
+			   const cairo_point_t *point);
+
+static inline cairo_int_status_t
+_cairo_contour_add_point (cairo_contour_t *contour,
+			  const cairo_point_t *point)
+{
+    struct _cairo_contour_chain *tail = contour->tail;
+
+    if (unlikely (tail->num_points == tail->size_points))
+	return __cairo_contour_add_point (contour, point);
+
+    tail->points[tail->num_points++] = *point;
+    return CAIRO_INT_STATUS_SUCCESS;
+}
+
+static inline cairo_point_t *
+_cairo_contour_first_point (cairo_contour_t *c)
+{
+    return &c->chain.points[0];
+}
+
+static inline cairo_point_t *
+_cairo_contour_last_point (cairo_contour_t *c)
+{
+    return &c->tail->points[c->tail->num_points-1];
+}
+
+cairo_private void
+_cairo_contour_simplify (cairo_contour_t *contour, double tolerance);
+
+cairo_private void
+_cairo_contour_reverse (cairo_contour_t *contour);
+
+cairo_private cairo_int_status_t
+_cairo_contour_add (cairo_contour_t *dst,
+		    const cairo_contour_t *src);
+
+cairo_private cairo_int_status_t
+_cairo_contour_add_reversed (cairo_contour_t *dst,
+			     const cairo_contour_t *src);
+
+cairo_private void
+__cairo_contour_remove_last_chain (cairo_contour_t *contour);
+
+static inline void
+_cairo_contour_remove_last_point (cairo_contour_t *contour)
+{
+    if (contour->chain.num_points == 0)
+	return;
+
+    if (--contour->tail->num_points == 0)
+	__cairo_contour_remove_last_chain (contour);
+}
+
+cairo_private void
+_cairo_contour_reset (cairo_contour_t *contour);
+
+cairo_private void
+_cairo_contour_fini (cairo_contour_t *contour);
+
+cairo_private void
+_cairo_debug_print_contour (FILE *file, cairo_contour_t *contour);
+
+CAIRO_END_DECLS
+
+#endif /* CAIRO_CONTOUR_PRIVATE_H */

src/cairo-contour.c
@@ -1,0 +1,453 @@
+/*
+ * Copyright © 2004 Carl Worth
+ * Copyright © 2006 Red Hat, Inc.
+ * Copyright © 2008 Chris Wilson
+ * Copyright © 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 1.1 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file COPYING-LGPL-2.1; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA
+ * You should have received a copy of the MPL along with this library
+ * in the file COPYING-MPL-1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ * The Original Code is the cairo graphics library.
+ *
+ * The Initial Developer of the Original Code is Carl Worth
+ *
+ * Contributor(s):
+ *	Carl D. Worth <cworth@cworth.org>
+ *	Chris Wilson <chris@chris-wilson.co.uk>
+ */
+
+#include "cairoint.h"
+
+#include "cairo-error-private.h"
+#include "cairo-freelist-private.h"
+#include "cairo-combsort-private.h"
+#include "cairo-contour-private.h"
+
+void
+_cairo_contour_init (cairo_contour_t *contour,
+		     int direction)
+{
+    contour->direction = direction;
+    contour->chain.points = contour->embedded_points;
+    contour->chain.next = NULL;
+    contour->chain.num_points = 0;
+    contour->chain.size_points = ARRAY_LENGTH (contour->embedded_points);
+    contour->tail = &contour->chain;
+}
+
+cairo_int_status_t
+__cairo_contour_add_point (cairo_contour_t *contour,
+			  const cairo_point_t *point)
+{
+    cairo_contour_chain_t *tail = contour->tail;
+    cairo_contour_chain_t *next;
+
+    assert (tail->next == NULL);
+
+    next = _cairo_malloc_ab_plus_c (tail->size_points*2,
+				    sizeof (cairo_point_t),
+				    sizeof (cairo_contour_chain_t));
+    if (unlikely (next == NULL))
+	return _cairo_error (CAIRO_STATUS_NO_MEMORY);
+
+    next->size_points = tail->size_points*2;
+    next->num_points = 1;
+    next->points = (cairo_point_t *)(next+1);
+    next->next = NULL;
+    tail->next = next;
+    contour->tail = next;
+
+    next->points[0] = *point;
+    return CAIRO_INT_STATUS_SUCCESS;
+}
+
+static void
+first_inc (cairo_contour_t *contour,
+	   cairo_point_t **p,
+	   cairo_contour_chain_t **chain)
+{
+    if (*p == (*chain)->points + (*chain)->num_points) {
+	assert ((*chain)->next);
+	*chain = (*chain)->next;
+	*p = &(*chain)->points[0];
+    } else
+	++*p;
+}
+
+static void
+last_dec (cairo_contour_t *contour,
+	  cairo_point_t **p,
+	  cairo_contour_chain_t **chain)
+{
+    if (*p == (*chain)->points) {
+	cairo_contour_chain_t *prev;
+	assert (*chain != &contour->chain);
+	for (prev = &contour->chain; prev->next != *chain; prev = prev->next)
+	    ;
+	*chain = prev;
+	*p = &(*chain)->points[(*chain)->num_points-1];
+    } else
+	--*p;
+}
+
+void
+_cairo_contour_reverse (cairo_contour_t *contour)
+{
+    cairo_contour_chain_t *first_chain, *last_chain;
+    cairo_point_t *first, *last;
+
+    contour->direction = -contour->direction;
+
+    if (contour->chain.num_points <= 1)
+	return;
+
+    first_chain = &contour->chain;
+    last_chain = contour->tail;
+
+    first = &first_chain->points[0];
+    last = &last_chain->points[last_chain->num_points-1];
+
+    while (first != last) {
+	cairo_point_t p;
+
+	p = *first;
+	*first = *last;
+	*last = p;
+
+	first_inc (contour, &first, &first_chain);
+	last_dec (contour, &last, &last_chain);
+    }
+}
+
+cairo_int_status_t
+_cairo_contour_add (cairo_contour_t *dst,
+		    const cairo_contour_t *src)
+{
+    const cairo_contour_chain_t *chain;
+    cairo_int_status_t status;
+    int i;
+
+    for (chain = &src->chain; chain; chain = chain->next) {
+	for (i = 0; i < chain->num_points; i++) {
+	    status = _cairo_contour_add_point (dst, &chain->points[i]);
+	    if (unlikely (status))
+		return status;
+	}
+    }
+
+    return CAIRO_INT_STATUS_SUCCESS;
+}
+
+static inline cairo_bool_t
+iter_next (cairo_contour_iter_t *iter)
+{
+    if (iter->point == &iter->chain->points[iter->chain->size_points-1]) {
+	iter->chain = iter->chain->next;
+	if (iter->chain == NULL)
+	    return FALSE;
+
+	iter->point = &iter->chain->points[0];
+	return TRUE;
+    } else {
+	iter->point++;
+	return TRUE;
+    }
+}
+
+static cairo_bool_t
+iter_equal (const cairo_contour_iter_t *i1,
+	    const cairo_contour_iter_t *i2)
+{
+    return i1->chain == i2->chain && i1->point == i2->point;
+}
+
+static void
+iter_init (cairo_contour_iter_t *iter, cairo_contour_t *contour)
+{
+    iter->chain = &contour->chain;
+    iter->point = &contour->chain.points[0];
+}
+
+static void
+iter_init_last (cairo_contour_iter_t *iter, cairo_contour_t *contour)
+{
+    iter->chain = contour->tail;
+    iter->point = &contour->tail->points[contour->tail->num_points-1];
+}
+
+static const cairo_contour_chain_t *prev_const_chain(const cairo_contour_t *contour,
+						     const cairo_contour_chain_t *chain)
+{
+    const cairo_contour_chain_t *prev;
+
+    if (chain == &contour->chain)
+	return NULL;
+
+    for (prev = &contour->chain; prev->next != chain; prev = prev->next)
+	;
+
+    return prev;
+}
+
+cairo_int_status_t
+_cairo_contour_add_reversed (cairo_contour_t *dst,
+			     const cairo_contour_t *src)
+{
+    const cairo_contour_chain_t *last;
+    cairo_int_status_t status;
+    int i;
+
+    if (src->chain.num_points == 0)
+	return CAIRO_INT_STATUS_SUCCESS;
+
+    for (last = src->tail; last; last = prev_const_chain (src, last)) {
+	for (i = last->num_points-1; i >= 0; i--) {
+	    status = _cairo_contour_add_point (dst, &last->points[i]);
+	    if (unlikely (status))
+		return status;
+	}
+    }
+
+    return CAIRO_INT_STATUS_SUCCESS;
+}
+
+static cairo_uint64_t
+point_distance_sq (const cairo_point_t *p1,
+		   const cairo_point_t *p2)
+{
+    int32_t dx = p1->x - p2->x;
+    int32_t dy = p1->y - p2->y;
+    return _cairo_int32x32_64_mul (dx, dx) + _cairo_int32x32_64_mul (dy, dy);
+}
+
+#define DELETED(p) ((p)->x == INT_MIN && (p)->y == INT_MAX)
+#define MARK_DELETED(p) ((p)->x = INT_MIN, (p)->y = INT_MAX)
+
+static cairo_bool_t
+_cairo_contour_simplify_chain (cairo_contour_t *contour, const double tolerance,
+			       const cairo_contour_iter_t *first,
+			       const cairo_contour_iter_t *last)
+{
+    cairo_contour_iter_t iter, furthest;
+    uint64_t max_error;
+    int x0, y0;
+    int nx, ny;
+    int count;
+
+    iter = *first;
+    iter_next (&iter);
+    if (iter_equal (&iter, last))
+	return FALSE;
+
+    x0 = first->point->x;
+    y0 = first->point->y;
+    nx = last->point->y - y0;
+    ny = x0 - last->point->x;
+
+    count = 0;
+    max_error = 0;
+    do {
+	cairo_point_t *p = iter.point;
+	if (! DELETED(p)) {
+	    uint64_t d = (uint64_t)nx * (x0 - p->x) + (uint64_t)ny * (y0 - p->y);
+	    if (d * d > max_error) {
+		max_error = d * d;
+		furthest = iter;
+	    }
+	    count++;
+	}
+	iter_next (&iter);
+    } while (! iter_equal (&iter, last));
+    if (count == 0)
+	return FALSE;
+
+    if (max_error > tolerance * ((uint64_t)nx * nx + (uint64_t)ny * ny)) {
+	cairo_bool_t simplified;
+
+	simplified = FALSE;
+	simplified |= _cairo_contour_simplify_chain (contour, tolerance,
+						     first, &furthest);
+	simplified |= _cairo_contour_simplify_chain (contour, tolerance,
+						     &furthest, last);
+	return simplified;
+    } else {
+	iter = *first;
+	iter_next (&iter);
+	do {
+	    MARK_DELETED (iter.point);
+	    iter_next (&iter);
+	} while (! iter_equal (&iter, last));
+
+	return TRUE;
+    }
+}
+
+void
+_cairo_contour_simplify (cairo_contour_t *contour, double tolerance)
+{
+    cairo_contour_chain_t *chain;
+    cairo_point_t *last = NULL;
+    cairo_contour_iter_t iter, furthest;
+    cairo_bool_t simplified;
+    uint64_t max = 0;
+    int i;
+
+    if (contour->chain.num_points <= 2)
+	return;
+
+    tolerance = tolerance * CAIRO_FIXED_ONE;
+    tolerance *= tolerance;
+
+    /* stage 1: vertex reduction */
+    for (chain = &contour->chain; chain; chain = chain->next) {
+	for (i = 0; i < chain->num_points; i++) {
+	    if (last == NULL ||
+		point_distance_sq (last, &chain->points[i]) > tolerance) {
+		last = &chain->points[i];
+	    } else {
+		MARK_DELETED (&chain->points[i]);
+	    }
+	}
+    }
+
+    /* stage2: polygon simplification using Douglas-Peucker */
+    simplified = FALSE;
+    do {
+	last = &contour->chain.points[0];
+	iter_init (&furthest, contour);
+	max = 0;
+	for (chain = &contour->chain; chain; chain = chain->next) {
+	    for (i = 0; i < chain->num_points; i++) {
+		uint64_t d;
+
+		if (DELETED (&chain->points[i]))
+		    continue;
+
+		d = point_distance_sq (last, &chain->points[i]);
+		if (d > max) {
+		    furthest.chain = chain;
+		    furthest.point = &chain->points[i];
+		    max = d;
+		}
+	    }
+	}
+	assert (max);
+
+	simplified = FALSE;
+	iter_init (&iter, contour);
+	simplified |= _cairo_contour_simplify_chain (contour, tolerance,
+						     &iter, &furthest);
+
+	iter_init_last (&iter, contour);
+	if (! iter_equal (&furthest, &iter))
+	    simplified |= _cairo_contour_simplify_chain (contour, tolerance,
+							 &furthest, &iter);
+    } while (simplified);
+
+    iter_init (&iter, contour);
+    for (chain = &contour->chain; chain; chain = chain->next) {
+	int num_points = chain->num_points;
+	chain->num_points = 0;
+	for (i = 0; i < num_points; i++) {
+	    if (! DELETED(&chain->points[i])) {
+		if (iter.point != &chain->points[i])
+		    *iter.point = chain->points[i];
+		iter.chain->num_points++;
+		iter_next (&iter);
+	    }
+	}
+    }
+
+    if (iter.chain) {
+	cairo_contour_chain_t *next;
+
+	for (chain = iter.chain->next; chain; chain = next) {
+	    next = chain->next;
+	    free (chain);
+	}
+
+	iter.chain->next = NULL;
+	contour->tail = iter.chain;
+    }
+}
+
+void
+_cairo_contour_reset (cairo_contour_t *contour)
+{
+    _cairo_contour_fini (contour);
+    _cairo_contour_init (contour, contour->direction);
+}
+
+void
+_cairo_contour_fini (cairo_contour_t *contour)
+{
+    cairo_contour_chain_t *chain, *next;
+
+    for (chain = contour->chain.next; chain; chain = next) {
+	next = chain->next;
+	free (chain);
+    }
+}
+
+void
+_cairo_debug_print_contour (FILE *file, cairo_contour_t *contour)
+{
+    cairo_contour_chain_t *chain;
+    int num_points, size_points;
+    int i;
+
+    num_points = 0;
+    size_points = 0;
+    for (chain = &contour->chain; chain; chain = chain->next) {
+	num_points += chain->num_points;
+	size_points += chain->size_points;
+    }
+
+    fprintf (file, "contour: direction=%d, num_points=%d / %d\n",
+	     contour->direction, num_points, size_points);
+
+    num_points = 0;
+    for (chain = &contour->chain; chain; chain = chain->next) {
+	for (i = 0; i < chain->num_points; i++) {
+	    fprintf (file, "  [%d] = (%f, %f)\n",
+		     num_points++,
+		     _cairo_fixed_to_double (chain->points[i].x),
+		     _cairo_fixed_to_double (chain->points[i].y));
+	}
+    }
+}
+
+void
+__cairo_contour_remove_last_chain (cairo_contour_t *contour)
+{
+    cairo_contour_chain_t *chain;
+
+    if (contour->tail == &contour->chain)
+	return;
+
+    for (chain = &contour->chain; chain->next != contour->tail; chain = chain->next)
+	;
+    free (contour->tail);
+    contour->tail = chain;
+    chain->next = NULL;
+}

src/cairo-path-fixed.c
@@ -1128,7 +1128,7 @@ _cpf_curve_to (void		*closure,
     cairo_point_t *p0 = &cpf->current_point;
 
     if (! _cairo_spline_init (&spline,
-			      cpf->line_to,
+			      (cairo_spline_add_point_func_t)cpf->line_to,
 			      cpf->closure,
 			      p0, p1, p2, p3))
     {

src/cairo-path-in-fill.c
@@ -217,7 +217,7 @@ _cairo_in_fill_curve_to (void *closure,
 
     /* XXX Investigate direct inspection of the inflections? */
     if (! _cairo_spline_init (&spline,
-			      _cairo_in_fill_line_to,
+			      (cairo_spline_add_point_func_t)_cairo_in_fill_line_to,
 			      in_fill,
 			      &in_fill->current_point, b, c, d))
     {

src/cairo-path-stroke.c
@@ -1118,7 +1118,7 @@ _cairo_stroker_curve_to (void *closure,
 	_cairo_stroker_line_to;
 
     if (! _cairo_spline_init (&spline,
-			      line_to, stroker,
+			      (cairo_spline_add_point_func_t)line_to, stroker,
 			      &stroker->current_point, b, c, d))
     {
 	return line_to (closure, d);

src/cairo-polygon.c
@@ -38,12 +38,14 @@
 #include "cairoint.h"
 
 #include "cairo-boxes-private.h"
+#include "cairo-contour-private.h"
 #include "cairo-error-private.h"
 
 static void
 _cairo_polygon_add_edge (cairo_polygon_t *polygon,
 			 const cairo_point_t *p1,
-			 const cairo_point_t *p2);
+			 const cairo_point_t *p2,
+			 int dir);
 
 void
 _cairo_polygon_init (cairo_polygon_t *polygon,
@@ -131,12 +133,12 @@ _cairo_polygon_init_boxes (cairo_polygon_t *polygon,
 		    p1 = chunk->base[i].p1;
 		    p2.x = p1.x;
 		    p2.y = chunk->base[i].p2.y;
-		    _cairo_polygon_add_edge (polygon, &p1, &p2);
+		    _cairo_polygon_add_edge (polygon, &p1, &p2, 1);
 
 		    p1 = chunk->base[i].p2;
 		    p2.x = p1.x;
 		    p2.y = chunk->base[i].p1.y;
-		    _cairo_polygon_add_edge (polygon, &p1, &p2);
+		    _cairo_polygon_add_edge (polygon, &p1, &p2, 1);
 	    }
     }
 
@@ -178,12 +180,12 @@ _cairo_polygon_init_box_array (cairo_polygon_t *polygon,
 	p1 = boxes[i].p1;
 	p2.x = p1.x;
 	p2.y = boxes[i].p2.y;
-	_cairo_polygon_add_edge (polygon, &p1, &p2);
+	_cairo_polygon_add_edge (polygon, &p1, &p2, 1);
 
 	p1 = boxes[i].p2;
 	p2.x = p1.x;
 	p2.y = boxes[i].p1.y;
-	_cairo_polygon_add_edge (polygon, &p1, &p2);
+	_cairo_polygon_add_edge (polygon, &p1, &p2, 1);
     }
 
     return polygon->status;
@@ -406,20 +408,17 @@ _add_clipped_edge (cairo_polygon_t *polygon,
 static void
 _cairo_polygon_add_edge (cairo_polygon_t *polygon,
 			 const cairo_point_t *p1,
-			 const cairo_point_t *p2)
+			 const cairo_point_t *p2,
+			 int dir)
 {
-    int dir;
-
     /* drop horizontal edges */
     if (p1->y == p2->y)
 	return;
 
-    if (p1->y < p2->y) {
-	dir = 1;
-    } else {
+    if (p1->y > p2->y) {
 	const cairo_point_t *t;
 	t = p1, p1 = p2, p2 = t;
-	dir = -1;
+	dir = -dir;
     }
 
     if (polygon->num_limits) {
@@ -439,7 +438,7 @@ _cairo_polygon_add_external_edge (void *polygon,
 				  const cairo_point_t *p1,
 				  const cairo_point_t *p2)
 {
-    _cairo_polygon_add_edge (polygon, p1, p2);
+    _cairo_polygon_add_edge (polygon, p1, p2, 1);
     return _cairo_polygon_status (polygon);
 }
 
@@ -466,6 +465,29 @@ _cairo_polygon_add_line (cairo_polygon_t *polygon,
 	_add_clipped_edge (polygon, &line->p1, &line->p2, top, bottom, dir);
     } else
 	_add_edge (polygon, &line->p1, &line->p2, top, bottom, dir);
+
+    return polygon->status;
+}
+
+cairo_status_t
+_cairo_polygon_add_contour (cairo_polygon_t *polygon,
+			    const cairo_contour_t *contour)
+{
+    const struct _cairo_contour_chain *chain;
+    const cairo_point_t *prev = NULL;
+    int i;
+
+    if (contour->chain.num_points <= 1)
+	return CAIRO_INT_STATUS_SUCCESS;
+
+    prev = &contour->chain.points[0];
+    for (chain = &contour->chain; chain; chain = chain->next) {
+	for (i = 0; i < chain->num_points; i++) {
+	    _cairo_polygon_add_edge (polygon, prev, &chain->points[i],
+				     contour->direction);
+	    prev = &chain->points[i];
+	}
+    }
 
     return polygon->status;
 }

commit 3d482e266febcf7da75f5662e518380460068ce1

test: Exercise bug in joining together spline segments around cusps
Carl Worth demonstrated a glaring bug in the new stroking code,
introduced in commit 545f30856aac98199 (stroke: Convert the outlines
into contour and then into a polygon), whereby only a bevel join was
being used to connect segments around a sharp inflection point.

This adds the two examples he reported to the test suite.

test/Makefile.sources
@@ -21,6 +21,7 @@ test_sources = \
 	big-empty-triangle.c				\
 	big-little-box.c				\
 	big-little-triangle.c				\
+	bug-spline.c					\
 	big-trap.c					\
 	bilevel-image.c					\
 	bug-40410.c					\

test/bug-spline.c
@@ -1,0 +1,96 @@
+/* cc `pkg-config --cflags --libs cairo` cairo-spline-image.c -o cairo-spline-image */
+
+/* Copyright © 2005 Carl Worth
+ * Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "cairo-test.h"
+
+#define WIDE_LINE_WIDTH 160
+#define NARROW_LINE_WIDTH 2
+
+/* A spline showing bugs in the "contour-based stroking" in cairo 1.12 */
+static const struct spline {
+    struct { double x, y; } pt[5];
+    double line_width;
+    double rgba[4];
+} splines[] = {
+    {
+	{
+	    { 172.25, 156.185 },
+	    { 177.225, 164.06 },
+	    { 176.5, 157.5 },
+	    { 175.5, 159.5 },
+	},
+	WIDE_LINE_WIDTH,
+	{ 1, 1, 1, 1 },
+    },
+    {
+	{
+	    { 571.25, 247.185 },
+	    { 78.225, 224.06 },
+	    { 129.5, 312.5 },
+	    { 210.5, 224.5 },
+	},
+	NARROW_LINE_WIDTH,
+	{ 1, 0, 0, 1 },
+    }
+};
+#define NUM_SPLINES (sizeof(splines)/sizeof(splines[0]))
+
+static cairo_test_status_t
+draw (cairo_t *cr, int width, int height)
+{
+    unsigned n;
+
+    cairo_set_source_rgb (cr, 0, 0, 0);
+    cairo_paint (cr);
+
+    cairo_set_line_cap(cr, CAIRO_LINE_CAP_SQUARE);
+
+    for (n = 0; n < NUM_SPLINES; n++) {
+	cairo_set_line_width (cr, splines[n].line_width);
+	cairo_set_source_rgba (cr,
+			       splines[n].rgba[0],
+			       splines[n].rgba[1],
+			       splines[n].rgba[2],
+			       splines[n].rgba[3]);
+
+	cairo_move_to (cr, splines[n].pt[0].x, splines[n].pt[0].y);
+	cairo_curve_to (cr,
+			splines[n].pt[1].x, splines[n].pt[1].y,
+			splines[n].pt[2].x, splines[n].pt[2].y,
+			splines[n].pt[3].x, splines[n].pt[3].y);
+
+	cairo_stroke (cr);
+    }
+
+    return CAIRO_TEST_SUCCESS;
+}
+
+CAIRO_TEST (bug_spline,
+	    "Exercises a bug in the stroking of splines",
+	    "spline, stroke", /* keywords */
+	    NULL, /* requirements */
+	    300, 300,
+	    NULL, draw)