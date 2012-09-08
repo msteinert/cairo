558f0a5ab4a88937b8c212c13309d11cb1a101af
Chris Wilson <chris@chris-wilson.co.uk>
2012-09-08 15:12:58 +0100

    image: Check for an error surface before dereferencing the backend
    As the surface->backend will be NULL in such an error surface, and we
    may be legitimately doing boundary checks to reject the error surface.
    The alternative would be to set an explicit error surface backend.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=54664
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/cairo-image-surface-inline.h
+++ src/cairo-image-surface-inline.h
@@ -73,7 +73,8 @@ _cairo_image_surface_is_clone (cairo_image_surface_t *image)
 static inline cairo_bool_t
 _cairo_surface_is_image (const cairo_surface_t *surface)
 {
-    return surface->backend->type == CAIRO_SURFACE_TYPE_IMAGE;
+    /* _cairo_surface_nil sets a NULL backend so be safe */
+    return surface->backend && surface->backend->type == CAIRO_SURFACE_TYPE_IMAGE;
 }
 
 /**
