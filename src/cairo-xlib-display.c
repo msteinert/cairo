6a03ae5859a92d3d4dbe97c9d598700fbcf1d666
Benjamin Otte <otte@redhat.com>
2014-03-03 01:16:25 +0100

    xlib: Fix typo in documentation

--- src/cairo-xlib-display.c
+++ src/cairo-xlib-display.c
@@ -568,7 +568,7 @@ _cairo_xlib_display_has_gradients (cairo_device_t *device)
  *
  * Restricts all future Xlib surfaces for this devices to the specified version
  * of the RENDER extension. This function exists solely for debugging purpose.
- * It let's you find out how cairo would behave with an older version of
+ * It lets you find out how cairo would behave with an older version of
  * the RENDER extension.
  *
  * Use the special values -1 and -1 for disabling the RENDER extension.
